a9a92f3317d21b0e07bcd6c38894ed5b64f903c1
ElijahCoding <cuiyeqing960904@gmail.com>
2021-03-21 20:00:51 +0300

    jwt middleware built

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class JwtMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$this->verifyToken(request()->bearerToken())) {
+            return response()->json([
+                'message' => 'Unauthenticated User.'
+            ], 401);
+        }
+
+        return $next($request);
+    }
+
+    protected function verifyToken($token)
+    {
+        $response = Http::withToken($token)->get(config('services.auth.url') . '/api/auth/user');
+
+        return $response->successful();
+    }
+}
