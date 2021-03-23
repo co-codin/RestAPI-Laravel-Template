629036ba1ba87699c83a5de44e973f8088da6249
ElijahCoding <cuiyeqing960904@gmail.com>
2021-03-23 12:03:18 +0300

    building tests

---
+++ tests/Feature/Modules/Achievements/Web/ReadTest.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Tests\Feature\Modules\Achievements\Web;
+
+use Tests\TestCase;
+
+class ReadTest extends TestCase
+{
+
+}
---
+++ tests/Feature/Modules/Brands/Web/ReadTest.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Tests\Feature\Modules\Brands\Web;
+
+use Tests\TestCase;
+
+class ReadTest extends TestCase
+{
+
+}
