b27ee004510db7611f578113f6301394bd079db1
ElijahCoding <cuiyeqing960904@gmail.com>
2021-03-23 19:09:32 +0300

    read test

--- tests/Feature/Modules/Achievements/Admin/CreateTest.php
+++ tests/Feature/Modules/Achievements/Admin/CreateTest.php
@@ -3,21 +3,34 @@
 
 namespace Tests\Feature\Modules\Achievements\Admin;
 
+use Modules\Achievement\Models\Achievement;
 use Tests\TestCase;
 
 class CreateTest extends TestCase
 {
     public function test_unauthenticated_cannot_create_achievement()
     {
-        $response = $this->json('POST', route('admin.achievements.store'));
-
-//        dd(
-//            $response->json()
-//        );
+        //
     }
 
     public function test_authenticated_can_create_achievement()
     {
-        $token = $this->getToken();
+        $achievementData = Achievement::factory()->raw();
+
+        $response = $this->json('POST', route('admin.achievements.store'), $achievementData);
+
+        $response->status(201);
+        $response->assertJson([
+            'data' => [
+                'name' => $achievementData['name'],
+                'image' => $achievementData['image'],
+                'is_enabled' => $achievementData['is_enabled']
+            ]
+        ]);
+        $this->assertDatabaseHas('achievements', [
+            'name' => $achievementData['name'],
+            'image' => $achievementData['image'],
+            'is_enabled' => $achievementData['is_enabled']
+        ]);
     }
 }
--- tests/Feature/Modules/Achievements/Admin/ReadTest.php
+++ tests/Feature/Modules/Achievements/Admin/ReadTest.php
@@ -7,6 +7,11 @@ use Tests\TestCase;
 
 class ReadTest extends TestCase
 {
+    public function test_unauthenticated_user_cannot_view_all_achievements()
+    {
+
+    }
+
     public function test_authenticated_user_can_view_all_achievements()
     {
 
