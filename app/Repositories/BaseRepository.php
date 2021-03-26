bfee8438ba0dd469a0272a454696097d8561b368
Lenar Khusainov <work@lenarx.ru>
2021-03-26 11:49:24 +0300

    make base repository abstract

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -2,16 +2,11 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
 /**
  * Class BaseRepository
  * @method jsonPaginate()
  */
-class BaseRepository extends \Prettus\Repository\Eloquent\BaseRepository
+abstract class BaseRepository extends \Prettus\Repository\Eloquent\BaseRepository
 {
-    public function model()
-    {
-        return Model::class;
-    }
+
 }
