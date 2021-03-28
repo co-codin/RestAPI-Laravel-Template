c52feb6187847da3fdc6d3b4db22c31fbe66c57c
Lenar Khusainov <work@lenarx.ru>
2021-03-28 12:26:48 +0300

    add query builder brands

--- Modules/Brand/Repositories/Criteria/BrandRequestCriteria.php
+++ Modules/Brand/Repositories/Criteria/BrandRequestCriteria.php
@@ -13,7 +13,7 @@ class BrandRequestCriteria implements CriteriaInterface
     {
         return QueryBuilder::for($model::query())
             ->defaultSort('id')
-            ->allowedFields(['id', 'name', 'slug', 'image', 'website', 'country', 'status', 'in_home', 'position'])
+            ->allowedFields(['id', 'name', 'slug', 'image', 'website', 'country', 'status', 'in_home', 'position', 'created_at', 'updated_at', 'deleted_at'])
             ->allowedFilters([
                 AllowedFilter::exact('id'),
                 AllowedFilter::partial('name'),
@@ -25,7 +25,7 @@ class BrandRequestCriteria implements CriteriaInterface
                 AllowedFilter::exact('is_in_home'),
                 AllowedFilter::exact('position'),
             ])
-            ->allowedSorts('id', 'name', 'slug', 'image', 'website', 'country', 'status', 'in_home', 'position')
+            ->allowedSorts('id', 'created_at', 'updated_at', 'name', 'slug', 'image', 'website', 'country', 'status', 'in_home', 'position')
             ;
     }
 }
