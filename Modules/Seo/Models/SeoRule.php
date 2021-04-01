93ae60ce9b8fb6a69c64f6018c5d0db63133df7b
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-01 13:20:40 +0300

    relationship added

--- Modules/Seo/Models/SeoRule.php
+++ Modules/Seo/Models/SeoRule.php
@@ -5,10 +5,11 @@ namespace Modules\Seo\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Seo\Database\factories\SeoRuleFactory;
+use Modules\Seo\Models\Traits\SeoBaseRelations;
 
 class SeoRule extends Model
 {
-    use HasFactory;
+    use HasFactory, SeoBaseRelations;
 
     protected $guarded = ['id'];
 
