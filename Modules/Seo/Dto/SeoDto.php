e7602a90e685f933229ef10f16ec4f3f62d1e1eb
Lenar Khusainov <work@lenarx.ru>
2021-04-01 17:26:35 +0300

    clear seo dto

--- Modules/Seo/Dto/SeoDto.php
+++ Modules/Seo/Dto/SeoDto.php
@@ -14,10 +14,6 @@ use Modules\Seo\Enums\SeoType;
  */
 class SeoDto extends Dto
 {
-    public string $seoable_type;
-
-    public int $seoable_int;
-
     public bool $is_enabled;
 
     public string $title;
