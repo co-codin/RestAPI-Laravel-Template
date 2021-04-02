4c82849cddcbd66327b3647ba8b6e2a9a0221121
Lenar Khusainov <work@lenarx.ru>
2021-04-02 09:22:27 +0300

    add category mixin to doc block

--- Modules/Category/Http/Resources/CategoryResource.php
+++ Modules/Category/Http/Resources/CategoryResource.php
@@ -4,7 +4,13 @@ namespace Modules\Category\Http\Resources;
 
 use App\Enums\Status;
 use App\Transformers\BaseJsonResource;
+use Modules\Category\Models\Category;
 
+/**
+ * Class CategoryResource
+ * @package Modules\Category\Http\Resources
+ * @mixin Category
+ */
 class CategoryResource extends BaseJsonResource
 {
     public function toArray($request): array
