b4e0146800999e0cc85c11a35e783908e18cf6ca
Lenar Khusainov <work@lenarx.ru>
2021-04-02 09:20:19 +0300

    change method to put

--- Modules/Achievement/Routes/admin.php
+++ Modules/Achievement/Routes/admin.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Modules\Achievement\Http\Controllers\Admin\AchievementController;
 
 Route::resource('achievements', AchievementController::class);
-Route::post('achievements/sort', [AchievementController::class, 'sort'])->name('achievements.sort');
+Route::put('achievements/sort', [AchievementController::class, 'sort'])->name('achievements.sort');
