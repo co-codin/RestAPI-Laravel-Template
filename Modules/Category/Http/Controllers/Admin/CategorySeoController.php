915cfc2716642dcba7160fef43348b095acb289d
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-02 15:42:10 +0300

    updated

--- Modules/Category/Http/Controllers/Admin/CategorySeoController.php
+++ Modules/Category/Http/Controllers/Admin/CategorySeoController.php
@@ -7,7 +7,6 @@ namespace Modules\Category\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Modules\Category\Repositories\CategoryRepository;
 use Modules\Seo\Dto\SeoDto;
-use Modules\Seo\Enums\SeoType;
 use Modules\Seo\Http\Requests\SeoUpdateRequest;
 use Modules\Seo\Http\Resources\SeoResource;
 use Modules\Seo\Services\SeoStorage;
@@ -19,31 +18,15 @@ class CategorySeoController extends Controller
         protected CategoryRepository $categoryRepository,
     ) {}
 
-    public function parentUpdate(SeoUpdateRequest $request, int $parent_category)
+    public function parentUpdate(SeoUpdateRequest $request, int $category)
     {
-        $parent_category = $this->categoryRepository->skipCriteria()->find($parent_category);
+        $category = $this->categoryRepository->skipCriteria()->find($category);
 
-        if ($parent_category->seo->type === SeoType::Self) {
-            $seo = $this->seoStorage->update(
-                $parent_category->seo(),
-                new SeoDto($request->validated())
-            );
+        $seo = $this->seoStorage->update(
+            $category->seo(),
+            new SeoDto($request->validated())
+        );
 
-            return new SeoResource($seo);
-        }
-    }
-
-    public function childUpdate(SeoUpdateRequest $request, int $child_category)
-    {
-        $child_category = $this->categoryRepository->skipCriteria()->find($child_category);
-
-        if ($child_category->seo->type === SeoType::Children) {
-            $seo = $this->seoStorage->update(
-                $child_category->seo(),
-                new SeoDto($request->validated())
-            );
-
-            return new SeoResource($seo);
-        }
+        return new SeoResource($seo);
     }
 }
--- Modules/Category/Routes/admin.php
+++ Modules/Category/Routes/admin.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\Category\Http\Controllers\Admin\CategoryController;
 use Modules\Category\Http\Controllers\Admin\CategorySeoController;
 
-Route::patch('categories/{parent_category}/seo', [CategorySeoController::class, 'parentUpdate']);
-Route::patch('categories/{child_category}/seo', [CategorySeoController::class, 'childUpdate']);
+Route::patch('categories/{category}/seo', [CategorySeoController::class, 'update']);
 
 Route::resource('categories', CategoryController::class);
