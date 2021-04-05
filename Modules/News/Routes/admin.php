fda7b319e35722be47cae4fa24285f2754950561
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-05 13:20:39 +0300

    seo

--- Modules/News/Routes/admin.php
+++ Modules/News/Routes/admin.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\News\Http\Controllers\Admin\NewsController;
+use Modules\News\Http\Controllers\Admin\NewsSeoController;
 
+Route::patch('news/{news}/seo', [NewsSeoController::class, 'update']);
 
 Route::resource('news', NewsController::class);
