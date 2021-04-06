c8b878ebd697dd501f5a4b7cbdbc4031d13ae388
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-06 15:52:54 +0300

    news tested

--- tests/Feature/Modules/News/Admin/CreateTest.php
+++ tests/Feature/Modules/News/Admin/CreateTest.php
@@ -4,9 +4,31 @@
 namespace Tests\Feature\Modules\News\Admin;
 
 
+use Modules\News\Models\News;
 use Tests\TestCase;
 
 class CreateTest extends TestCase
 {
+    public function test_unauthenticated_cannot_create_news()
+    {
+        //
+    }
 
+    public function test_authenticated_can_create_news()
+    {
+        $newsData = News::factory()->raw();
+
+        $response = $this->json('POST', route('admin.news.store'), $newsData);
+
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'data' => [
+                'name',
+                'slug',
+            ]
+        ]);
+        $this->assertDatabaseHas('news', [
+            'name' => $newsData['name'],
+        ]);
+    }
 }
--- tests/Feature/Modules/News/Admin/UpdateTest.php
+++ tests/Feature/Modules/News/Admin/UpdateTest.php
@@ -4,9 +4,34 @@
 namespace Tests\Feature\Modules\News\Admin;
 
 
+use App\Enums\Status;
+use Modules\News\Models\News;
 use Tests\TestCase;
 
 class UpdateTest extends TestCase
 {
+    public function test_unauthenticated_cannot_update_news()
+    {
+        //
+    }
 
+    public function test_authenticated_can_update_news()
+    {
+        $this->withoutExceptionHandling();
+
+        $news = News::factory()->create([
+            'status' => Status::ACTIVE,
+        ]);
+
+        $response = $this->json('PATCH', route('admin.news.update', ['news' => $news->id]), [
+            'name' => $newName = 'new name',
+            'status' => Status::INACTIVE
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('news', [
+            'name' => $newName,
+            'status' => Status::INACTIVE
+        ]);
+    }
 }
