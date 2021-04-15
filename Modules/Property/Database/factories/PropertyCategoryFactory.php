7bf0b3427b3dd52151db42901983bbdb8dc03822
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-15 17:23:19 +0300

    building structure

---
+++ Modules/Property/Database/factories/PropertyCategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+namespace Modules\Property\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PropertyCategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \Modules\Property\Models\PropertyCategory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
+
