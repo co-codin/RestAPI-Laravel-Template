992d922e0ed975c668755bfea31c700f789e7cb8
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-18 00:57:49 +0300

    public rest api bilt

--- Modules/Property/Http/Controllers/PropertyController.php
+++ Modules/Property/Http/Controllers/PropertyController.php
@@ -4,16 +4,26 @@
 namespace Modules\Property\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Modules\Property\Http\Resources\PropertyResource;
+use Modules\Property\Repositories\PropertyRepository;
 
 class PropertyController extends Controller
 {
+    public function __construct(
+        protected PropertyRepository $propertyRepository
+    ) {}
+
     public function index()
     {
+        $properties = $this->propertyRepository->jsonPaginate();
 
+        return PropertyResource::collection($properties);
     }
 
-    public function show()
+    public function show(int $property)
     {
+        $property = $this->propertyRepository->find($property);
 
+        return new PropertyResource($property);
     }
 }
--- Modules/Property/Routes/admin.php
+++ Modules/Property/Routes/admin.php
@@ -1,1 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Property\Http\Controllers\Admin\PropertyController;
+
+Route::resource('properties', PropertyController::class)->except(['index', 'show']);
