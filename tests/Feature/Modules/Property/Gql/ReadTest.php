b7f6588b98f76cad421904b8d7131f481ee2ff5d
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-19 15:23:36 +0300

    where has tested

--- tests/Feature/Modules/Property/Gql/ReadTest.php
+++ tests/Feature/Modules/Property/Gql/ReadTest.php
@@ -3,6 +3,7 @@
 
 namespace Tests\Feature\Modules\Property\Gql;
 
+use Modules\Category\Models\Category;
 use Modules\Property\Models\Property;
 use Tests\TestCase;
 
@@ -72,7 +73,46 @@ class ReadTest extends TestCase
     public function test_properties_can_be_filtered_by_category_id()
     {
         $property = Property::factory()->create();
+        $anotherProperty = Property::factory()->create();
+        $category = Category::factory()->create();
 
+        $property->categories()->sync($category);
 
+        $response = $this->graphQL('
+            {
+                properties(hasCategories: { column: ID, operator: EQ, value: ' . $category->id .'  }) {
+                    data {
+                        id
+                        name
+                    }
+                }
+            }
+        ');
+
+        $response->assertJson([
+            'data' => [
+                'properties' => [
+                    'data' => [
+                        [
+                            'id' => $property->id,
+                            'name' => $property->name,
+                        ]
+                    ],
+                ]
+            ],
+        ]);
+
+        $response->assertJsonMissing([
+            'data' => [
+                'properties' => [
+                    'data' => [
+                        [
+                            'id' => $anotherProperty->id,
+                            'name' => $anotherProperty->name,
+                        ]
+                    ],
+                ]
+            ],
+        ]);
     }
 }
