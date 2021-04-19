1c3f9585afd47fd629d00789411aa549edc34266
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-19 17:49:21 +0300

    fixed question categories

--- Modules/Faq/Routes/admin.php
+++ Modules/Faq/Routes/admin.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Modules\Faq\Http\Controllers\Admin\QuestionCategoryController;
 use Modules\Faq\Http\Controllers\Admin\QuestionController;
 
-Route::post('question_categories/sort', [QuestionCategoryController::class, 'sort']);
-Route::resource('question_categories', QuestionCategoryController::class)->except(['index', 'show']);
+Route::post('question-categories/sort', [QuestionCategoryController::class, 'sort']);
+Route::resource('question-categories', QuestionCategoryController::class)->except(['index', 'show']);
 
 Route::post('questions/sort', [QuestionController::class, 'sort']);
 Route::resource('questions', QuestionController::class)->except(['index', 'show']);
