0400a45661476fe558842c52fcae2d89a8a05f46
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-19 12:51:52 +0300

    todo added

--- Modules/Property/Database/Migrations/2021_04_16_072437_create_property_value_table.php
+++ Modules/Property/Database/Migrations/2021_04_16_072437_create_property_value_table.php
@@ -15,14 +15,16 @@ class CreatePropertyValueTable extends Migration
     {
         Schema::create('property_value', function (Blueprint $table) {
             $table->primary(['property_id']);
+            // TODO module Product
 //            $table->primary(['property_id', 'product_id']);
             $table->foreignId('property_id')->constrained();
+            // TODO module Product
 //            $table->foreignId('product_id')->constrained();
             $table->json('value')->nullable();
             $table->string('pretty_key')->nullable();
             $table->string('pretty_value')->nullable();
             $table->boolean('is_important')->default(false);
-            $table->tinyInteger('important_position')->default(0);
+            $table->tinyInteger('important_position')->default(null);
             $table->string('important_value')->nullable();
         });
     }
