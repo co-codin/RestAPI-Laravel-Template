2461ecc6be4bc706237b21ffb121bdfd53f09598
Lenar Khusainov <work@lenarx.ru>
2021-04-19 17:16:10 +0300

    Add unsigneds to integer columns

--- Modules/Property/Database/Migrations/2021_04_16_072437_create_property_value_table.php
+++ Modules/Property/Database/Migrations/2021_04_16_072437_create_property_value_table.php
@@ -24,7 +24,7 @@ class CreatePropertyValueTable extends Migration
             $table->string('pretty_key')->nullable();
             $table->string('pretty_value')->nullable();
             $table->boolean('is_important')->default(false);
-            $table->tinyInteger('important_position')->default(null);
+            $table->unsignedTinyInteger('important_position')->default(null);
             $table->string('important_value')->nullable();
         });
     }
