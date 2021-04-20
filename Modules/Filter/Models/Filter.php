63facc47311ddcc438c80bfb1177522021917f03
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-20 20:56:59 +0300

    gql built

--- Modules/Filter/Models/Filter.php
+++ Modules/Filter/Models/Filter.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Category\Models\Category;
 use Modules\Filter\Database\factories\FilterFactory;
+use Modules\Property\Models\Property;
 
 class Filter extends Model
 {
@@ -24,6 +25,11 @@ class Filter extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
     }
 
     protected static function newFactory()
