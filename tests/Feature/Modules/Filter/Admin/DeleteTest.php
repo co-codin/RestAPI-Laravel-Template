04ba104b507df43fa4b3eb04c01c8a77d95003da
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-21 14:15:32 +0300

    delete test built

---
+++ tests/Feature/Modules/Filter/Admin/DeleteTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Tests\Feature\Modules\Filter\Admin;
+
+use Modules\Filter\Models\Filter;
+use Tests\TestCase;
+
+class DeleteTest extends TestCase
+{
+    public function test_unauthenticated_cannot_delete_filter()
+    {
+        //
+    }
+
+    public function test_authenticated_can_delete_filter()
+    {
+        $filter = Filter::factory()->create();
+
+        $response = $this->deleteJson(route('admin.filters.destroy', $filter));
+
+        $response->assertNoContent();
+
+        $this->assertDeleted($filter);
+    }
+}
