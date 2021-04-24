e7e9bd8a3dab68a3294cbd759d482ba960dca83b
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-24 16:05:09 +0300

    admin api built

--- Modules/Currency/Http/Controllers/Admin/CurrencyController.php
+++ Modules/Currency/Http/Controllers/Admin/CurrencyController.php
@@ -5,7 +5,10 @@ namespace Modules\Currency\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use Modules\Currency\Dto\CurrencyDto;
 use Modules\Currency\Http\Requests\CurrencyCreateRequest;
+use Modules\Currency\Http\Requests\CurrencyUpdateRequest;
+use Modules\Currency\Http\Resources\CurrencyResource;
 use Modules\Currency\Repositories\CurrencyRepository;
 use Modules\Currency\Services\CurrencyStorage;
 
@@ -18,16 +21,26 @@ class CurrencyController extends Controller
 
     public function store(CurrencyCreateRequest $request)
     {
+        $currency = $this->currencyStorage->store(CurrencyDto::fromFormRequest($request));
 
+        return new CurrencyResource($currency);
     }
 
-    public function update()
+    public function update(int $currency, CurrencyUpdateRequest $request)
     {
+        $currencyModel = $this->currencyRepository->find($currency);
 
+        $currencyModel = $this->currencyStorage->update($currencyModel, CurrencyDto::fromFormRequest($request));
+
+        return new CurrencyResource($currencyModel);
     }
 
-    public function destroy()
+    public function destroy(int $currency)
     {
+        $currencyModel = $this->currencyRepository->find($currency);
 
+        $this->currencyStorage->delete($currencyModel);
+
+        return response()->noContent();
     }
 }
--- Modules/Currency/Services/CurrencyStorage.php
+++ Modules/Currency/Services/CurrencyStorage.php
@@ -4,7 +4,28 @@
 namespace Modules\Currency\Services;
 
 
+use Modules\Currency\Dto\CurrencyDto;
+use Modules\Currency\Models\Currency;
+
 class CurrencyStorage
 {
+    public function store(CurrencyDto $currencyDto)
+    {
+        return Currency::query()->create($currencyDto->toArray());
+    }
 
+    public function update(Currency $currency, CurrencyDto $currencyDto)
+    {
+        if (!$currency->update($currencyDto->toArray())) {
+            throw new \LogicException('can not update currency.');
+        }
+        return $currency;
+    }
+
+    public function delete(Currency $currency)
+    {
+        if (!$currency->delete()) {
+            throw new \LogicException('can not delete currency.');
+        }
+    }
 }
