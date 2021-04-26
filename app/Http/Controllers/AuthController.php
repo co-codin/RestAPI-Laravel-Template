16b0ce03ec0120e551a2835a6caf59ed86a22b93
Lenar Khusainov <work@lenarx.ru>
2021-04-26 23:02:59 +0300

    Fix changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
