cbd7203ead35e0af6df6605d273511d0aa437a2c
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-26 21:24:31 +0300

    spatie query built

--- Modules/Product/Repositories/Criteria/ProductRequestCriteria.php
+++ Modules/Product/Repositories/Criteria/ProductRequestCriteria.php
@@ -18,9 +18,28 @@ class ProductRequestCriteria implements CriteriaInterface
             ->allowedFields(['id', 'name', 'image', 'position', 'is_enabled', 'created_at', 'updated_at'])
             ->allowedFilters([
                 AllowedFilter::exact('id'),
+                AllowedFilter::exact('slug'),
                 AllowedFilter::partial('name'),
+                AllowedFilter::exact('brand_id'),
+                AllowedFilter::exact('status'),
                 AllowedFilter::exact('image'),
-                AllowedFilter::exact('is_enabled'),
+                AllowedFilter::exact('is_in_home'),
+                AllowedFilter::exact('warranty'),
+                AllowedFilter::partial('short_description'),
+                AllowedFilter::partial('full_description'),
+
+                AllowedFilter::trashed(),
+
+                AllowedFilter::exact('categories.id'),
+
+                AllowedFilter::exact('productVariants.id'),
+                AllowedFilter::partial('productVariants.name'),
+                AllowedFilter::exact('productVariants.is_price_visible'),
+                AllowedFilter::exact('productVariants.is_enabled'),
+                AllowedFilter::exact('productVariants.currency_id'),
+                AllowedFilter::exact('productVariants.price'),
+                AllowedFilter::exact('productVariants.availability'),
+                AllowedFilter::exact('productVariants.previous_price'),
             ])
             ->allowedSorts('id', 'name', 'warranty', 'created_at', 'updated_at', 'deleted_at')
             ;
