ae4ffc4d1a414d87591c1626df15204a299fcc16
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-26 17:10:41 +0300

    requests built

---
+++ Modules/Product/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Modules\Product\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+}
--- Modules/Product/Routes/admin.php
+++ Modules/Product/Routes/admin.php
@@ -1,1 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Product\Http\Controllers\Admin\ProductController;
+
+Route::resource('products', ProductController::class)->except(['index', 'show']);
