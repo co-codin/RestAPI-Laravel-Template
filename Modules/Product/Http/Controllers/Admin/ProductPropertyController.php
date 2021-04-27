b6bc5fc060ace104023a8c5e04697313b05e04b8
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-27 15:46:37 +0300

    product property update method

--- Modules/Product/Http/Controllers/Admin/ProductPropertyController.php
+++ Modules/Product/Http/Controllers/Admin/ProductPropertyController.php
@@ -5,11 +5,23 @@ namespace Modules\Product\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use Modules\Product\Http\Requests\ProductPropertyUpdateRequest;
+use Modules\Product\Repositories\ProductRepository;
+use Modules\Product\Services\ProductPropertyStorage;
 
 class ProductPropertyController extends Controller
 {
-    public function update()
-    {
+    public function __construct(
+        protected ProductPropertyStorage $productPropertyStorage,
+        protected ProductRepository $productRepository
+    ){}
 
+    public function update(int $product, ProductPropertyUpdateRequest $request)
+    {
+        $productModel = $this->productRepository->find($product);
+
+        $this->productPropertyStorage->update($productModel, $request->get('properties'));
+
+        return response()->noContent();
     }
 }
--- Modules/Product/Routes/admin.php
+++ Modules/Product/Routes/admin.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Product\Http\Controllers\Admin\ProductController;
+use Modules\Product\Http\Controllers\Admin\ProductPropertyController;
 
+Route::put('products/{product}/properties', [ProductPropertyController::class, 'update'])->name('product.property.update');
 Route::resource('products', ProductController::class)->except(['index', 'show']);
