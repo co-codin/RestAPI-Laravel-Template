a6ccc896d7e757084d6365daea81f5de356ede08
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-27 15:50:44 +0300

    built

--- Modules/Product/Routes/admin.php
+++ Modules/Product/Routes/admin.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Product\Http\Controllers\Admin\ProductController;
 use Modules\Product\Http\Controllers\Admin\ProductPropertyController;
+use Modules\Product\Http\Controllers\Admin\ProductSeoController;
 
+Route::patch('products/{product}/seo', [ProductSeoController::class, 'update']);
 Route::put('products/{product}/properties', [ProductPropertyController::class, 'update'])->name('product.property.update');
 Route::resource('products', ProductController::class)->except(['index', 'show']);
