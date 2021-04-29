2fed4b056e637870d0648195823c6d568413c5da
ElijahCoding <cuiyeqing960904@gmail.com>
2021-04-29 13:01:47 +0300

    update method built

--- tests/Feature/Modules/Product/Admin/Configurator/UpdateTest.php
+++ tests/Feature/Modules/Product/Admin/Configurator/UpdateTest.php
@@ -3,11 +3,11 @@
 
 namespace Tests\Feature\Modules\Product\Admin\Configurator;
 
-use Modules\Currency\Models\Currency;
 use Modules\Product\Enums\ProductVariantStock;
 use Modules\Product\Models\Product;
 use Modules\Product\Models\ProductVariant;
 use Tests\TestCase;
+use function React\Promise\map;
 
 class UpdateTest extends TestCase
 {
@@ -49,10 +49,38 @@ class UpdateTest extends TestCase
     public function test_authenticated_can_update_configurator()
     {
 
+        $product = Product::factory()->create();
+
+        $productVariant = ProductVariant::factory()->create([
+            'product_id' => $product->id,
+        ]);
+
+        $response = $this->json(
+            'PUT',
+            route('admin.product.configurator.update', $product),
+            [
+                'variants' => [
+                    [
+                        'id' => $productVariant->id,
+                        'name' => $newName = 'new_name',
+                        'is_price_visible' => true,
+                        'is_enabled' => true,
+                        'availability' => ProductVariantStock::ComingSoon,
+                    ]
+                ],
+            ],
+        );
+
+        $response->assertNoContent();
+
+        $this->assertDatabaseHas('product_variants', [
+            'product_id' => $product->id,
+            'name' => $newName,
+        ]);
     }
 
     public function test_authenticated_can_create_and_update_configurator()
     {
-        
+
     }
 }
