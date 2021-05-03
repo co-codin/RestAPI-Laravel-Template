963f6fe323f0ee447d4ffc19548489b177db7ed9
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-03 14:30:15 +0300

    update method built

--- tests/Feature/Modules/Product/Admin/Product/UpdateTest.php
+++ tests/Feature/Modules/Product/Admin/Product/UpdateTest.php
@@ -4,6 +4,9 @@
 namespace Tests\Feature\Modules\Product\Admin\Product;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Modules\Product\Enums\DocumentSource;
+use Modules\Product\Enums\DocumentType;
 use Modules\Product\Models\Product;
 use Tests\TestCase;
 
@@ -38,11 +41,38 @@ class UpdateTest extends TestCase
 
         $response = $this->json('PATCH', route('admin.products.update', $product), [
             'name' => $newName = 'new name',
+            'documents' => $documents = [
+                [
+                    'name' => 'test',
+                    'source' => DocumentSource::URL,
+                    'url' => 'http://www.test.com',
+                    'type' => DocumentType::MANUAL,
+                    'position' => 1
+                ],
+                [
+                    'name' => 'test_2',
+                    'source' => DocumentSource::FILE,
+                    'file' => $file = UploadedFile::fake()->createWithContent('test.pdf', 'test'),
+                    'type' => DocumentType::MANUAL,
+                ],
+            ]
         ]);
 
         $response->assertOk();
         $this->assertDatabaseHas('products', [
             'name' => $newName,
         ]);
+
+        $response->assertJson(fn(AssertableJson $json) => $json
+            ->has('data.documents', count($documents))
+            ->has('data.documents.0', fn($json) => $json->where('name', 'test')
+                ->where('url', 'http://www.test.com')
+                ->etc()
+            )
+            ->has('data.documents.1', fn($json) => $json->where('name', 'test_2')
+                ->where('file', date('Y') . '/' . date('m') . '/' . $file->hashName())
+                ->etc()
+            )
+        );
     }
 }
