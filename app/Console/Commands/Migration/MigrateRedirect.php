265031a2d89e4f5f79098cd6919e49ff75c4f89b
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-11 14:52:14 +0300

    redirect built

--- app/Console/Commands/Migration/MigrateRedirect.php
+++ app/Console/Commands/Migration/MigrateRedirect.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands\Migration;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Modules\Redirect\Models\Redirect;
 
 class MigrateRedirect extends Command
 {
@@ -12,6 +14,27 @@ class MigrateRedirect extends Command
 
     public function handle()
     {
-        //
+        $oldRedirects = DB::connection('old_medeq_mysql')
+            ->table('redirects')
+            ->get();
+
+
+        foreach ($oldRedirects as $oldRedirect) {
+            Redirect::query()->insert(
+                $this->transform($oldRedirect)
+            );
+        }
+    }
+
+    protected function transform($item)
+    {
+        return [
+            'id' => $item->id,
+            'source' => $item->old_url,
+            'destination' => $item->new_url,
+            'code' => $item->code,
+            'created_at' => $item->created_at,
+            'updated_at' => $item->updated_at,
+        ];
     }
 }
