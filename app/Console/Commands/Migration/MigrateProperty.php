7071062e546b307d1f53f20e3e477f04a20bfe17
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-12 10:44:03 +0300

    property migrated

--- app/Console/Commands/Migration/MigrateProperty.php
+++ app/Console/Commands/Migration/MigrateProperty.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands\Migration;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Modules\Property\Models\Property;
 
 class MigrateProperty extends Command
 {
@@ -12,6 +14,29 @@ class MigrateProperty extends Command
 
     public function handle()
     {
-        //
+        $oldProperties = DB::connection('old_medeq_mysql')
+            ->table('properties')
+            ->get();
+
+        foreach ($oldProperties as $oldProperty) {
+            Property::query()->insert(
+                $this->transform($oldProperty)
+            );
+        }
+    }
+
+    protected function transform($item)
+    {
+        return [
+            'id' => $item->id,
+            'name' => $item->title,
+            'type' => $item->type,
+            'options' => $item->options,
+            'description' => $item->description,
+            'is_hidden_from_product' => $item->hide_from_product,
+            'is_hidden_from_comparison' => $item->hide_from_comparison,
+            'created_at' => $item->created_at,
+            'updated_at' => $item->updated_at,
+        ];
     }
 }
