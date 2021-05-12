49d7940fe47120b903af05906d964a065c2b5807
dmitry <d.bormisov@medeqstars.ru>
2021-05-12 16:27:16 +0300

    MQ-1790 fix

--- Modules/Seo/Http/Requests/Admin/CanonicalUpdateRequest.php
+++ Modules/Seo/Http/Requests/Admin/CanonicalUpdateRequest.php
@@ -14,16 +14,6 @@ use Symfony\Component\HttpFoundation\Response;
 class CanonicalUpdateRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
