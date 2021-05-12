b25d073c41640ca38d77e76ab1738cd93bf1610c
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-12 12:10:55 +0300

    built

--- app/Console/Commands/Migration/MigratePropertyValue.php
+++ app/Console/Commands/Migration/MigratePropertyValue.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\Migration;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class MigratePropertyValue extends Command
 {
@@ -10,8 +11,42 @@ class MigratePropertyValue extends Command
 
     protected $description = 'Migrate property value';
 
+    protected $propertyValues;
+
+    protected $bookItems;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->propertyValues = DB::connection('old_medeq_mysql')->table('property_values')->get();
+        $this->bookItems = DB::connection('old_medeq_mysql')->table('book_items')->get();
+    }
+
     public function handle()
     {
+        foreach ($this->propertyValues as $propertyValue) {
+            DB::table('property_value')->insert(
+                $this->transform($propertyValue)
+            );
+        }
+    }
 
+    protected function transform($item)
+    {
+        return [
+            'property_id' => $item->property_id,
+            'product_id' => $item->product_id,
+            'pretty_key' => $item->specification_key,
+            'pretty_value' => $item->specification_value,
+            'value' => $item->value ? $this->getBookItem($item->value) : null,
+        ];
+    }
+
+    protected function getBookItem($id)
+    {
+        if (!$bookItem = $this->bookItems->where('id', (int) $id)->first()) {
+            return null;
+        }
+        return json_encode((array) $bookItem);
     }
 }
