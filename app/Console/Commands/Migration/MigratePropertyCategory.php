e87d0ffc8355714ffb0230107bb0961f9a01a1d6
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-12 11:17:46 +0300

    property category migrated

--- app/Console/Commands/Migration/MigratePropertyCategory.php
+++ app/Console/Commands/Migration/MigratePropertyCategory.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands\Migration;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Modules\Achievement\Models\Achievement;
 
 class MigratePropertyCategory extends Command
 {
@@ -12,6 +14,14 @@ class MigratePropertyCategory extends Command
 
     public function handle()
     {
+        $propertyCategories = DB::connection('old_medeq_mysql')
+            ->table('property_categories')
+            ->get();
 
+        foreach ($propertyCategories as $propertyCategory) {
+            DB::table('property_category')->insert(
+                (array) $propertyCategory
+            );
+        }
     }
 }
