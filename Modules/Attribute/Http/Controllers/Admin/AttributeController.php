ccb76300edd6b7a106ff19d895b0dbc6af8d56de
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-17 13:48:31 +0300

    structure built

---
+++ Modules/Attribute/Http/Controllers/Admin/AttributeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Attribute\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class AttributeController extends Controller
+{
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
---
+++ Modules/Attribute/Repositories/AttributeRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Modules\Attribute\Repositories;
+
+
+use App\Repositories\BaseRepository;
+use Modules\Attribute\Models\Attribute;
+
+class AttributeRepository extends BaseRepository
+{
+    public function model()
+    {
+        return Attribute::class;
+    }
+
+    public function boot()
+    {
+        
+    }
+}
