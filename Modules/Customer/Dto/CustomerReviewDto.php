53c1ce4c5c9ee9bbbf95522e713c55a1716c2eaa
dmitry <d.bormisov@medeqstars.ru>
2021-05-19 23:23:51 +0300

    MQ-1481 fix dto

--- Modules/Customer/Dto/CustomerReviewDto.php
+++ Modules/Customer/Dto/CustomerReviewDto.php
@@ -6,6 +6,7 @@ namespace Modules\Customer\Dto;
 
 use App\Dto\BaseDto;
 use Illuminate\Http\UploadedFile;
+use Modules\Customer\Enums\CustomerType;
 
 /**
  * Class CustomerReviewDto
@@ -13,12 +14,12 @@ use Illuminate\Http\UploadedFile;
  */
 class CustomerReviewDto extends BaseDto
 {
-    public string $post;
-    public string $author;
-    public int $type;
+    public ?string $post;
+    public ?string $author;
+    public int $type = CustomerType::PrivatePerson;
     public ?string $video;
     public UploadedFile|string|null $review_file;
-    public bool $is_home;
-    public string $comment;
+    public bool $is_home = false;
+    public ?string $comment;
     public UploadedFile|string|null $logo;
 }
