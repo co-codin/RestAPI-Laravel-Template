f319b3f0a27b02abae8b31b14b29f74de12dbb9a
dmitry <d.bormisov@medeqstars.ru>
2021-05-19 13:39:20 +0300

    MQ-1481 add tests

---
+++ tests/Feature/Modules/Customer/CustomerReview/Admin/CreateTest.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Tests\Feature\Modules\Customer\CustomerReview\Admin;
+
+use Modules\Customer\Models\CustomerReview;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+//    public function test_unauthenticated_cannot_create_customer_review()
+//    {
+//        //
+//    }
+
+    public function test_authenticated_can_create_customer_review()
+    {
+        $customerReviewData = CustomerReview::factory()->raw();
+
+        $response = $this->json('POST', route('admin.customer-reviews.store'), $customerReviewData);
+
+        $response->assertCreated();
+        $response->assertJsonStructure([
+            'data' => [
+                'post',
+                'author',
+                'type',
+                'video',
+                'review_file',
+                'is_home',
+                'comment',
+                'logo',
+            ]
+        ]);
+
+        $this->assertDatabaseHas('customer_reviews', [
+            'post' => $customerReviewData['post'],
+            'author' => $customerReviewData['author'],
+            'type' => $customerReviewData['type'],
+            'video' => $customerReviewData['video'],
+            'review_file' => $customerReviewData['review_file'],
+            'is_home' => $customerReviewData['is_home'],
+            'comment' => $customerReviewData['comment'],
+            'logo' => $customerReviewData['logo'],
+        ]);
+    }
+}
---
+++ tests/Feature/Modules/Customer/CustomerReview/Web/ReadTest.php
@@ -0,0 +1,85 @@
+<?php
+
+
+namespace Tests\Feature\Modules\Customer\CustomerReview\Web;
+
+use Modules\Customer\Models\CustomerReview;
+use Tests\TestCase;
+use function route;
+
+class ReadTest extends TestCase
+{
+    public function test_user_can_view_customer_reviews()
+    {
+        CustomerReview::factory()->count($count = 5)->create();
+
+        $response = $this->json('GET', route('customer-reviews.index'));
+
+        $response->assertOk();
+        $this->assertCount($count, ($response['data']));
+
+        $response->assertJsonStructure([
+            'data' => [
+                [
+                    "id",
+                    "post",
+                    "author",
+                    "type",
+                    "video",
+                    "review_file",
+                    "is_home",
+                    "comment",
+                    "logo",
+                    "created_at",
+                    "updated_at",
+                ]
+            ],
+            'links' => [
+                "first",
+                "last",
+                "prev",
+                "next",
+            ],
+            'meta' => [
+                'current_page',
+                'from',
+                'last_page',
+                'links' => [
+                    [
+                        'url',
+                        'label',
+                        'active',
+                    ]
+                ],
+                'path',
+                'per_page',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    public function test_user_can_view_single_customer_review()
+    {
+        $customerReview = CustomerReview::factory()->create();
+
+        $response = $this->json('GET', route('customer-reviews.show', $customerReview));
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'data' => [
+                "id",
+                "post",
+                "author",
+                "type",
+                "video",
+                "review_file",
+                "is_home",
+                "comment",
+                "logo",
+                "created_at",
+                "updated_at",
+            ]
+        ]);
+    }
+}
