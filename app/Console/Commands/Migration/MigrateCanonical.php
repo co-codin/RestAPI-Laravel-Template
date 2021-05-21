abef679ac5132fb9386d34dd1382ddb0d17527d6
dmitry <d.bormisov@medeqstars.ru>
2021-05-21 14:18:55 +0300

    MQ-1790 migrate canonical

---
+++ app/Console/Commands/Migration/MigrateCanonical.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands\Migration;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Modules\Seo\Models\CanonicalEntity;
+
+class MigrateCanonical extends Command
+{
+    protected $signature = 'migrate:canonical';
+    protected $description = 'Migrate canonicals';
+
+    public function handle()
+    {
+        $oldCanonicals = DB::connection('old_medeq_mysql')
+            ->table('canonicals')
+            ->get();
+
+        CanonicalEntity::query()->insert($oldCanonicals->toArray());
+    }
+}
