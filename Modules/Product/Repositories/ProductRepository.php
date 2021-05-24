8950fb7ab1cab74e0c5cf04fe6851c34a1030125
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-24 17:13:29 +0300

    product query refactored

--- Modules/Product/Repositories/ProductRepository.php
+++ Modules/Product/Repositories/ProductRepository.php
@@ -7,6 +7,7 @@ namespace Modules\Product\Repositories;
 use App\Enums\Status;
 use App\Repositories\BaseRepository;
 use Modules\Product\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Modules\Product\Repositories\Criteria\ProductRequestCriteria;
 
 class ProductRepository extends BaseRepository
@@ -26,48 +27,23 @@ class ProductRepository extends BaseRepository
         $data = Product::query()->select([
             'id', 'name', 'slug', 'status', 'brand_id'
         ])
-        ->where('status', '=', Status::ACTIVE);
-
-        return Product::query()->fromQuery("
-                SELECT
-                    p.id,
-                    p.name,
-                    p.slug,
-                    p.status,
-                    p.brand_id
-                FROM
-                    products AS p
-                WHERE
-                    p.status = " . Status::ACTIVE . "
-                    AND EXISTS (
-                        SELECT
-                            1
-                        FROM
-                            product_variations AS pv
-                        WHERE
-                            pv.product_id = p.id
-                            AND pv.status = " . Status::ACTIVE . "
-                        )
-                    AND EXISTS (
-                        SELECT
-                            1
-                        FROM
-                            product_category AS pc
-                            JOIN categories AS cat ON cat.id = pc.category_id
-                        WHERE
-                            pc.product_id = p.id
-                            AND cat.status = " . Status::ACTIVE . "
-                        )
-                    AND EXISTS (
-                        SELECT
-                            1
-                        FROM
-                            brands AS b
-                        WHERE
-                            p.brand_id = b.id
-                            AND b.status = " . Status::ACTIVE . "
-                        )
-        ")
+            ->where('status', '=', Status::ACTIVE)
+            ->whereHas('productVariations', function (Builder $query) {
+                $query->where('status', '=', Status::ACTIVE);
+            })
+            ->whereHas('category', function (Builder $query) {
+                $query->where('status', '=', Status::ACTIVE);
+            })
+            ->whereHas('brand', function (Builder $query) {
+                $query->where('status', '=', Status::ACTIVE);
+            })
+            ->with([
+                'categories',
+                'brand',
+                'productVariations.currency',
+                'properties',
+                'category',
+            ])
             ->get();
     }
 }
