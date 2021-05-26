d275862f079615cca93713d4fedab1fa857c2df1
ElijahCoding <cuiyeqing960904@gmail.com>
2021-05-26 23:41:51 +0300

    refactoring

--- app/Console/Commands/Migration/MigratePropertyValue.php
+++ app/Console/Commands/Migration/MigratePropertyValue.php
@@ -15,15 +15,19 @@ class MigratePropertyValue extends Command
 
     protected $bookItems;
 
+    protected $properties;
+
     public function handle()
     {
         $this->propertyValues = DB::connection('old_medeq_mysql')->table('property_values')->get();
         $this->bookItems = DB::connection('old_medeq_mysql')->table('book_items')->get();
+        $this->properties = DB::connection('old_medeq_mysql')->table('properties')->get();
 
         foreach ($this->propertyValues as $propertyValue) {
-            DB::table('property_value')->insert(
-                $this->transform($propertyValue)
-            );
+            dump($propertyValue->value);
+//            DB::table('property_value')->insert(
+//                $this->transform($propertyValue)
+//            );
         }
     }
 
@@ -34,15 +38,19 @@ class MigratePropertyValue extends Command
             'product_id' => $item->product_id,
             'pretty_key' => $item->specification_key,
             'pretty_value' => $item->specification_value,
-            'value' => $item->value ? $this->getBookItem($item->value) : null,
+            'value' => $item->value ? $this->getBookItem($item->value, $item->property_id) : null,
         ];
     }
 
-    protected function getBookItem($id)
+    protected function getBookItem($id, $property_id)
     {
-        if (!$bookItem = $this->bookItems->where('id', (int) $id)->first()) {
+        if (
+            $bookItem = $this->bookItems->where('id', (int) $id)->first() &&
+                $this->properties->where('id', '=', (int) $property_id)->first()->type == 4
+        ) {
+            return json_encode((array) $bookItem)['title'];
+        } else {
             return null;
         }
-        return json_encode((array) $bookItem);
     }
 }
