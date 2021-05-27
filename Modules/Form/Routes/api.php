ff126c790cf25449829c20c6e4d737967d41d783
dmitry <d.bormisov@medeqstars.ru>
2021-05-27 19:53:35 +0300

    MQ-1480 why not? new route with dynamic actions

--- Modules/Form/Routes/api.php
+++ Modules/Form/Routes/api.php
@@ -3,8 +3,17 @@
 use App\Http\Middleware\ClientAuth;
 use Modules\Form\Http\Controllers\FormController;
 
-Route::prefix('form')->group(function() {
-    Route::post('/{formName}/send', [FormController::class, 'send'])
+Route::prefix('form')->group(function () {
+    Route::post('/{formName}/send', function (string $formName) {
+        $controller = App::make(Modules\Form\Http\Controllers\FormController::class);
+        $container = \Illuminate\Container\Container::getInstance();
+
+        if (method_exists(FormController::class, Str::camel($formName))) {
+            return $container->call([$controller, Str::camel($formName)]);
+        }
+
+        return $container->call([$controller, 'send']);
+    })
         ->middleware(ClientAuth::class)
         ->where('formName', '[a-z0-9_-]+')
         ->name('form-send');
