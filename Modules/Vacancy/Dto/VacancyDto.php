775cd758e415657ebf8c3c5a4dc6926a868e93f0
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-03 14:16:14 +0300

    dto built

--- Modules/Vacancy/Dto/VacancyDto.php
+++ Modules/Vacancy/Dto/VacancyDto.php
@@ -8,5 +8,13 @@ use App\Dto\BaseDto;
 
 class VacancyDto extends BaseDto
 {
+    public ?string $name;
 
+    public ?string $slug;
+
+    public ?string $short_description;
+
+    public ?string $full_description;
+
+    public ?int $status;
 }
