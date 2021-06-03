808c293f88c971dd4a564b6eb7a9971f933001ba
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-03 12:53:15 +0300

    controllers building

---
+++ Modules/Vacancy/Http/Controllers/Admin/VacancyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Vacancy\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class VacancyController extends Controller
+{
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
