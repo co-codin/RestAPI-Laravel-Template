2fd412810b9802eb8a9c1f6bc979af1a8cff0e66
dmitry <d.bormisov@medeqstars.ru>
2021-06-08 19:24:07 +0300

    MQ-1835 add custom validate rule service provider

---
+++ app/Providers/CustomValidateRuleServiceProvider.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Validator as ValidationValidator;
+
+class CustomValidateRuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Validator::extend('integer_keys', function ($attribute, $value, $parameters, $validator) {
+            return is_array($value) && count(array_filter(array_keys($value), 'is_string')) === 0;
+        });
+
+        Validator::extend('external_links', function ($attribute, $value, $parameters, $validator) {
+            $regular = '~(?!http:\/\/medeqstars\.ru|https:\/\/medeqstars\.ru|http:\/\/medeq\.ru|https:\/\/medeq\.ru)(([-\w]+\.)'
+                . '+(com|net|org|info|biz|com.ru|org.ru|net.ru|ru|su|us|bz|kz|'
+                . 'ws|рф|pro|li|cc|me|mi|in|is|to|ly|gd|click|lt|download|diet|'
+                . 'work|tokyo|racing|science|party|faith|москва|рус|xyz|bar|shop)+(\s|$))(?<!medeqstars\.ru\s|medeq\.ru\s)~ui';
+
+            preg_match_all($regular, $value, $externalLinks, PREG_SET_ORDER);
+
+            $re = '/'
+                . '(?!http:\/\/medeqstars\.ru|https:\/\/medeqstars\.ru|http:\/\/medeq\.ru|https:\/\/medeq\.ru)'
+                . '(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?'
+                . '(?:'
+                . '(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})'
+                . '(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})'
+                . '(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}'
+                . '(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))'
+                . '|'
+                . '(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)'
+                . '(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,}))'
+                . ')'
+                . '(?::\d{2,5})?(?:\/[^\s]*)?(?<!medeqstars\.ru|medeq\.ru)'
+                . '/iu';
+
+            preg_match_all($re, $value, $externalLinksDiegoperini, PREG_SET_ORDER);
+
+            return !($externalLinks || $externalLinksDiegoperini);
+        });
+
+        Validator::extend('is_youtube_link', function ($attribute, string $value, $parameters, $validator) {
+            $re = '/https:\/\/(?:www\.)?youtu\.be\/[a-zA-Z0-9_=-]+$/';
+
+            preg_match($re, $value, $matches);
+
+            if (!empty($matches)) {
+                return true;
+            }
+
+            return false;
+        });
+
+        Validator::extend('distinct_concrete_keys', function (
+            string $attribute,
+            $value,
+            array $parameters,
+            ValidationValidator $validator
+        ) {
+            $attributeName = substr(strrchr($attribute, '.'), 1 );
+
+            foreach ($parameters as $param) {
+                $duplicateValues = collect($validator->getData())
+                    ->filter(function (array $variation) use ($param, $attributeName) {
+                        return $variation[$attributeName] == $param;
+                    })
+                    ->count();
+
+                if ($duplicateValues > 1) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+    }
+}
--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
 
         \Nuwave\Lighthouse\WhereConditions\WhereConditionsServiceProvider::class,
         \App\Providers\SpatieQueryBuilderServiceProvider::class,
+        App\Providers\CustomValidateRuleServiceProvider::class,
     ],
 
     /*
