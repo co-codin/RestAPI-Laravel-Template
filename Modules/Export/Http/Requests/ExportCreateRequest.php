2aa0ac3342e958027f71d9525bd238123ac7f711
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-15 22:04:45 +0300

    export requests built

--- Modules/Export/Http/Requests/ExportCreateRequest.php
+++ Modules/Export/Http/Requests/ExportCreateRequest.php
@@ -2,7 +2,10 @@
 
 namespace Modules\Export\Http\Requests;
 
+use BenSampo\Enum\Rules\EnumValue;
 use Illuminate\Foundation\Http\FormRequest;
+use Modules\Export\Enum\ExportType;
+use Modules\Product\Enums\ProductVariationStock;
 
 class ExportCreateRequest extends FormRequest
 {
@@ -14,7 +17,42 @@ class ExportCreateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'type' => [
+                'required',
+                'integer',
+                new EnumValue(ExportType::class, false),
+            ],
+            'filename' => 'required|string|max:255',
+            'frequency' => 'required|integer',
+            'parameters' => 'required|array',
+
+            'parameters.categories' => 'required|array',
+            'parameters.categories.ids' => 'sometimes|array',
+            'parameters.categories.ids.*' => 'sometimes|integer|exists:categories,id',
+            'parameters.categories.selected' => 'sometimes|bool',
+
+            'parameters.brands' => 'required|array',
+            'parameters.brands.ids' => 'sometimes|array',
+            'parameters.brands.ids.*' => 'sometimes|integer|exists:brands,id',
+            'parameters.brands.selected' => 'sometimes|boolean',
+
+            'parameters.products' => 'required|array',
+            'parameters.products.ids' => 'sometimes|array',
+            'parameters.products.ids.*' => 'sometimes|integer|exists:brands,id',
+            'parameters.products.selected' => 'sometimes|boolean',
+
+            'parameters.stock_type' => 'sometimes|string|max:255',
+
+            'parameters.in_stock' => [
+                'sometimes',
+                'integer',
+                new EnumValue(ProductVariationStock::class, false),
+            ],
+
+            'parameters.short_description' => 'sometimes|boolean',
+
+            'parameters.price' => 'sometimes|boolean'
         ];
     }
 }
