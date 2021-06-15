c983e51fb1ea2c09371e9fd8524915c29a8c55a5
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-15 16:00:14 +0300

    building create update delete

--- Modules/Export/Http/Controllers/Admin/ExportController.php
+++ Modules/Export/Http/Controllers/Admin/ExportController.php
@@ -5,20 +5,29 @@ namespace Modules\Export\Http\Controllers\Admin;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Export\Http\Requests\ExportCreateRequest;
+use Modules\Export\Http\Requests\ExportUpdateRequest;
+use Modules\Export\Repositories\ExportRepository;
+use Modules\Export\Services\ExportStorage;
 
 class ExportController extends Controller
 {
-    public function store()
+    public function __construct(
+        protected ExportRepository $exportRepository,
+        protected ExportStorage $exportStorage
+    ) {}
+
+    public function store(ExportCreateRequest $request)
     {
 
     }
 
-    public function update()
+    public function update(int $export, ExportUpdateRequest $request)
     {
 
     }
 
-    public function destroy()
+    public function destroy(int $export)
     {
 
     }
