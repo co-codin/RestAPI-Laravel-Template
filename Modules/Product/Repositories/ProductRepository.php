78da266a48eda572e3e492e0c5a732f0b5ce9069
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-16 15:28:06 +0300

    getProductsForMerchant built

--- Modules/Product/Repositories/ProductRepository.php
+++ Modules/Product/Repositories/ProductRepository.php
@@ -25,6 +25,43 @@ class ProductRepository extends BaseRepository
         $this->pushCriteria(ProductRequestCriteria::class);
     }
 
+    public function getProductsForMerchant($withPrice)
+    {
+        return Product::select([
+            'products.id',
+            'products.name',
+            'products.slug',
+            'products.brand_id',
+            'products.image',
+            'products.short_description'
+        ])
+            ->with([
+                'productVariations.currency',
+                'category',
+                'brand',
+                'images',
+                'properties',
+            ])
+            ->where('products.status', Status::ACTIVE)
+            ->whereHas('productVariations', function ($query) use ($withPrice) {
+                $query->where('product_variations.is_enabled', true);
+
+                if ($withPrice) {
+                    $query->where('product_variations.is_price_visible', true);
+                } else {
+                    $query->where(function($q) {
+                        $q->orWhere('product_variations.is_price_visible', false)
+                            ->orWhere('product_variations.price', null);
+                    });
+                }
+            })
+            ->whereHas('brand', function ($query) {
+                $query->where('brands.status', Status::ACTIVE);
+            })
+            ->whereNotNull('short_description')
+            ->get();
+    }
+
     public function indexForProducts()
     {
         return Product::query()
