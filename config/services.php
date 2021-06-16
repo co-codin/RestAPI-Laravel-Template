ffcc8deb3e6b9caca89015b4deaf3e2bc93be0b4
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-16 14:12:26 +0300

    services config added

--- config/services.php
+++ config/services.php
@@ -29,10 +29,37 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+
     'auth' => [
         'url' => env('AUTH_URL'),
     ],
+
     'content' => [
         'domain' => env('CONTENT_API_URL', 'https://content.api.medeq.ru'),
+    ],
+
+    'yandex-market' => [
+        'filename' => env('YANDEX_MARKET_FILENAME', 'ymlmarket'),
+    ],
+
+    'tiu' => [
+        'with_price' => [
+            'filename' => env('TIU_MARKET_FILENAME_WITH_PRICE', 'tiumarket_with_price'),
+        ],
+        'without_price' => [
+            'filename' => env('TIU_MARKET_FILENAME_WITHOUT_PRICE', 'tiumarket_without_price'),
+        ],
+        'company_name' => env('APP_NAME')
+    ],
+
+    'google-market' => [
+        'filename' => env('GOOGLE_MARKET_FILENAME', 'googlemarket'),
+        'company_name' => env('APP_NAME'),
+        'link' => env('APP_URL'),
+        'description' => env('GOOGLE_MARKET_DESCRIPTION', 'Best store'),
+    ],
+
+    'facebook-market' => [
+        'filename' => env('FACEBOOK_MARKET_FILENAME', 'facebookmarket'),
     ],
 ];
