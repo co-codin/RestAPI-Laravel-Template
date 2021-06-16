d7bb8bfe3e5e288b1daceeea88794686a498f7ab
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-16 13:41:59 +0300

    update method tested

--- tests/Feature/Modules/Export/Admin/UpdateTest.php
+++ tests/Feature/Modules/Export/Admin/UpdateTest.php
@@ -4,32 +4,27 @@
 namespace Tests\Feature\Modules\Export\Admin;
 
 
-use App\Enums\Status;
-use Modules\News\Models\News;
+use Modules\Export\Models\Export;
 use Tests\TestCase;
 
 class UpdateTest extends TestCase
 {
-//    public function test_unauthenticated_cannot_update_news()
+//    public function test_unauthenticated_cannot_update_export()
 //    {
 //        //
 //    }
 
-    public function test_authenticated_can_update_news()
+    public function test_authenticated_can_update_export()
     {
-        $news = News::factory()->create([
-            'status' => Status::ACTIVE,
-        ]);
+        $export = Export::factory()->create();
 
-        $response = $this->json('PATCH', route('admin.news.update', $news), [
+        $response = $this->json('PATCH', route('admin.exports.update', $export), [
             'name' => $newName = 'new name',
-            'status' => Status::INACTIVE
         ]);
 
         $response->assertOk();
-        $this->assertDatabaseHas('news', [
+        $this->assertDatabaseHas('exports', [
             'name' => $newName,
-            'status' => Status::INACTIVE
         ]);
     }
 }
