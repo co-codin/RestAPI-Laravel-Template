1049b94532c1cda71135e61ddb8765dd374b7305
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-17 20:16:48 +0300

    dynamic merchant built

--- Modules/Product/Repositories/ProductRepository.php
+++ Modules/Product/Repositories/ProductRepository.php
@@ -7,6 +7,7 @@ namespace Modules\Product\Repositories;
 use App\Enums\Status;
 use App\Repositories\BaseRepository;
 use Illuminate\Support\Arr;
+use Modules\Product\Enums\ProductVariationStock;
 use Modules\Product\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
 use Modules\Product\Repositories\Criteria\ProductRequestCriteria;
@@ -53,15 +54,15 @@ class ProductRepository extends BaseRepository
         }
 
         if (array_key_exists('in_stock', $parameters)) {
-            $in_stock = Arr::get($parameters, 'in_stock');
+            $in_stock = (bool) Arr::get($parameters, 'in_stock');
         }
 
         if (array_key_exists('short_description', $parameters)) {
-            $short_description = Arr::get($parameters, 'short_description');
+            $short_description = (bool) Arr::get($parameters, 'short_description');
         }
 
         if (array_key_exists('price', $parameters)) {
-            $price = Arr::get($parameters, 'price');
+            $price = (bool) Arr::get($parameters, 'price');
         }
 
         $query = Product::query();
@@ -84,42 +85,42 @@ class ProductRepository extends BaseRepository
             'properties',
         ])->where('status', '=', Status::ACTIVE);
 
-        $query->whereHas()
+        $query->whereHas('brand', function ($query) use ($brands) {
+            $query->where('brands.status', Status::ACTIVE);
+        });
 
+        if ($brands) {
+            $query->whereIn('brand_id', $brands);
+        }
 
-        return Product::select([
-            'products.id',
-            'products.name',
-            'products.slug',
-            'products.brand_id',
-            'products.image',
-            'products.short_description'
-        ])
-            ->with([
-                'productVariations.currency',
-                'category',
-                'brand',
-                'images',
-                'properties',
-            ])
-            ->where('products.status', Status::ACTIVE)
-            ->whereHas('productVariations', function ($query) use ($withPrice) {
-                $query->where('product_variations.is_enabled', true);
+        if ($categories) {
+            $query->whereHas('categories', function ($query) use ($categories) {
+                $query->whereIn('id', $categories);
+            });
+        }
 
-                if ($withPrice) {
-                    $query->where('product_variations.is_price_visible', true);
-                } else {
-                    $query->where(function($q) {
-                        $q->orWhere('product_variations.is_price_visible', false)
-                            ->orWhere('product_variations.price', null);
-                    });
-                }
-            })
-            ->whereHas('brand', function ($query) {
-                $query->where('brands.status', Status::ACTIVE);
-            })
-            ->whereNotNull('short_description')
-            ->get();
+        $query->whereHas('productVariations', function ($query) use ($price, $in_stock, $stock_type) {
+            $query->where('product_variations.is_enabled', '=', true);
+
+            if ($price) {
+                $query->where('product_variations.is_price_visible', '=', true);
+            } else {
+                $query->where(function($q) {
+                    $q->orWhere('product_variations.is_price_visible', '=', false)
+                        ->orWhere('product_variations.price', '=', null);
+                });
+            }
+
+            if ($in_stock) {
+                $query->where('product_variations.availability', '=', ProductVariationStock::InStock);
+            }
+
+            $query->where('product_variations.stock_type', $stock_type ? '=' : '!=', null);
+        });
+
+        $query->where('short_description', $short_description ? '=' : '!=', null);
+
+        return $query->get();
     }
 
     public function indexForProducts()
