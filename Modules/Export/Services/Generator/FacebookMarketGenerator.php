515f5a8a452e27277b67a9100f498057edb008cf
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-17 17:24:03 +0300

    fetching variation

--- Modules/Export/Services/Generator/FacebookMarketGenerator.php
+++ Modules/Export/Services/Generator/FacebookMarketGenerator.php
@@ -5,6 +5,7 @@ namespace Modules\Export\Services\Generator;
 
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Modules\Product\Repositories\ProductRepository;
 
 class FacebookMarketGenerator implements GeneratorInterface
@@ -28,8 +29,35 @@ class FacebookMarketGenerator implements GeneratorInterface
         }
     }
 
-    public function transform($data)
+    public function transform($product)
     {
-        return $data;
+        $productVariation = $product->productVariations->first();
+
+        return [
+            $product->id,
+            $product->name,
+            $product->short_description,
+            $this->getAvailabilityByMerchant($productVariation->availability),
+            'new',
+            $productVariation->price . ' ' . Str::upper($productVariation->currency->code),
+            route('product-view', [
+                'slug' => $product->slug,
+                'id' => $product->id,
+            ])
+        ];
+    }
+
+    private function getAvailabilityByMerchant($value)
+    {
+        switch ($value) {
+            case 1:
+                return 'in stock';
+                break;
+            case 2:
+                return 'available for order';
+                break;
+            default:
+                return 'out of stock';
+        }
     }
 }
