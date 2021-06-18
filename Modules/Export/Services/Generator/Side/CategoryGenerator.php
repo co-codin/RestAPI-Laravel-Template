14bfe9ad3f2c7d671b1a3d6a0b687bc672ebcd5e
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-18 14:22:41 +0300

    offer generator built, building category generator

--- Modules/Export/Services/Generator/Side/CategoryGenerator.php
+++ Modules/Export/Services/Generator/Side/CategoryGenerator.php
@@ -6,5 +6,25 @@ namespace Modules\Export\Services\Generator\Side;
 
 class CategoryGenerator
 {
+    public function getCategories(): array
+    {
+        $categories = [];
 
+        foreach ($this->getCategoryEntities() as $category) {
+            $categories[] = (new CategoryYaml())
+                ->setId($category->id)
+                ->setParentId($category->parent_id)
+                ->setName($category->title);
+        }
+
+        return $categories;
+    }
+
+    private function getCategoryEntities(): Collection
+    {
+        return Category::query()
+            ->defaultOrder()
+            ->whereStatus(Status::Active)
+            ->get();
+    }
 }
--- Modules/Export/Services/Generator/Side/OffersGenerator.php
+++ Modules/Export/Services/Generator/Side/OffersGenerator.php
@@ -4,6 +4,10 @@
 namespace Modules\Export\Services\Generator\Side;
 
 
+use Bukashk0zzz\YmlGenerator\Model\Offer\OfferParam;
+use Bukashk0zzz\YmlGenerator\Model\Offer\OfferSimple;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Modules\Product\Enums\ProductVariationStock;
 use Modules\Product\Repositories\ProductRepository;
 
@@ -16,6 +20,7 @@ class OffersGenerator
     public function getOffers(array $parameters): array
     {
         $offers = [];
+        $price = null;
 
         foreach ($this->productRepository->getProductsForMerchant($parameters) as $product) {
             $pictures = collect($product->image)
@@ -24,9 +29,39 @@ class OffersGenerator
 
             $variation = $product->productVariations->where('is_enabled', '=', true)->first();
 
+            $offer = (new OfferSimple())
+                ->setId($product->id)
+                ->setAvailable((int)$variation->in_stock === ProductVariationStock::InStock)
+                ->setPictures($pictures)
+                ->setDescription($product->short_description)
+                ->setUrl(route('product-view', [$product->slug, $product->id]))
+                ->setCategoryId($product->category->id)
+                ->setDelivery(true)
+                ->setName($product->brand->name . ' ' . $product->name)
+                ->setVendor($product->brand->name)
+                ->addCustomElement('typePrefix', $product->category->product_name)
+            ;
 
+            if (array_key_exists('price', $parameters)) {
+                $price = (bool) Arr::get($parameters, 'price');
+            }
 
+            if ($price) {
+                $offer->setPrice($variation->price);
+                $offer->setCurrencyId(Str::upper($variation->currency->iso_code));
+            }
 
+            foreach ($product->properties as $property) {
+                $offer->addParam(
+                    (new OfferParam())
+                        ->setName($property->name)
+                        ->setValue($property->pivot->value())
+                );
+            }
+
+            $offers[] = $offer;
         }
+
+        return $offers;
     }
 }
