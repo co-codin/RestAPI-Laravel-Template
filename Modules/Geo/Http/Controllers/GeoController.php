b2ed8dcce10558b0b73105c6bd9b1f5a64da7d27
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-07 15:47:40 -0400

    ip detect

--- Modules/Geo/Http/Controllers/GeoController.php
+++ Modules/Geo/Http/Controllers/GeoController.php
@@ -2,78 +2,20 @@
 
 namespace Modules\Geo\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Geo\Services\SxGeo;
 
 class GeoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-    public function index()
+    public function detectIp(Request $request)
     {
-        return view('geo::index');
-    }
+        $request->validate([
+            'ip' => 'required|ip'
+        ]);
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('geo::create');
-    }
+        $SxGeo = new SxGeo(storage_path('app/SxGeoCity.dat', SXGEO_BATCH | SXGEO_MEMORY));
 
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('geo::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('geo::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
+        return $SxGeo->getCity($request->get('ip'));
     }
 }
--- Modules/Geo/Routes/api.php
+++ Modules/Geo/Routes/api.php
@@ -1,2 +1,5 @@
 <?php
 
+use Modules\Geo\Http\Controllers\GeoController;
+
+Route::get('/detect-ip', GeoController::class)->name('geo.detect_ip');
