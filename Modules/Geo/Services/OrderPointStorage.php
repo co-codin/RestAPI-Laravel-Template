e6a1641bfdf0e96d2f1443d060aac97fc4901b71
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-09 14:11:54 -0400

    building order point crud

---
+++ Modules/Geo/Services/OrderPointStorage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Geo\Services;
+
+class OrderPointStorage
+{
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+        
+    }
+}
