99e0b530bac526c9cb1fcc04473b9adf75022631
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-09 03:08:41 -0400

    building sold product

---
+++ Modules/Geo/Database/Migrations/2021_08_09_100835_create_sold_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSoldProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sold_products', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sold_products');
+    }
+}
