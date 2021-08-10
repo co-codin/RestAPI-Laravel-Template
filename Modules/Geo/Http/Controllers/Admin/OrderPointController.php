c9463414afc2d921b6cb3028cdf8e376ee75c583
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-10 08:02:58 -0400

    testing

--- Modules/Geo/Http/Controllers/Admin/OrderPointController.php
+++ Modules/Geo/Http/Controllers/Admin/OrderPointController.php
@@ -3,26 +3,42 @@
 namespace Modules\Geo\Http\Controllers\Admin;
 
 use Illuminate\Routing\Controller;
+use Modules\Geo\Dto\OrderPointDto;
+use Modules\Geo\Http\Requests\OrderPointCreateRequest;
+use Modules\Geo\Http\Requests\OrderPointUpdateRequest;
+use Modules\Geo\Http\Resources\OrderPointResource;
+use Modules\Geo\Repositories\OrderPointRepository;
 use Modules\Geo\Services\OrderPointStorage;
 
 class OrderPointController extends Controller
 {
     public function __construct(
-        protected OrderPointStorage $orderPointStorage
+        protected OrderPointStorage $orderPointStorage,
+        protected OrderPointRepository $orderPointRepository
     ) {}
 
-    public function store()
+    public function store(OrderPointCreateRequest $request)
     {
+        $orderPoint = $this->orderPointStorage->store(OrderPointDto::fromFormRequest($request));
 
+        return new OrderPointResource($orderPoint);
     }
 
-    public function update()
+    public function update(int $order_point, OrderPointUpdateRequest $request)
     {
+        $order_point = $this->orderPointRepository->find($order_point);
 
+        $order_point = $this->orderPointStorage->update($order_point, OrderPointDto::fromFormRequest($request));
+
+        return new OrderPointResource($order_point);
     }
 
-    public function destroy()
+    public function destroy(int $order_point)
     {
+        $order_point = $this->orderPointRepository->find($order_point);
 
+        $this->orderPointStorage->delete($order_point);
+
+        return response()->noContent();
     }
 }
--- Modules/Geo/Services/OrderPointStorage.php
+++ Modules/Geo/Services/OrderPointStorage.php
@@ -2,20 +2,35 @@
 
 namespace Modules\Geo\Services;
 
+use Modules\Geo\Dto\OrderPointDto;
+use Modules\Geo\Models\OrderPoint;
+
 class OrderPointStorage
 {
-    public function store()
+    public function store(OrderPointDto $orderPointDto)
     {
+        $orderPoint = new OrderPoint($orderPointDto->toArray());
 
+        if (!$orderPoint->save()) {
+            throw new \LogicException('can not create order point.');
+        }
+
+        return $orderPoint;
     }
 
-    public function update()
+    public function update(OrderPoint $orderPoint, OrderPointDto $orderPointDto)
     {
+        if (!$orderPoint->update($orderPointDto->toArray())) {
+            throw new \LogicException('can not update order point.');
+        }
 
+        return $orderPoint;
     }
 
-    public function delete()
+    public function delete(OrderPoint $orderPoint)
     {
-
+        if (!$orderPoint->delete()) {
+            throw new \LogicException('can not delete order point.');
+        }
     }
 }
