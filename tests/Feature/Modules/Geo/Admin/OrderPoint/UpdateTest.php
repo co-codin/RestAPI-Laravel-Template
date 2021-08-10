3cead4b59eff206aa783075a96826974c1453de8
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-10 08:26:34 -0400

    built

--- tests/Feature/Modules/Geo/Admin/OrderPoint/UpdateTest.php
+++ tests/Feature/Modules/Geo/Admin/OrderPoint/UpdateTest.php
@@ -2,9 +2,27 @@
 
 namespace Tests\Feature\Modules\Geo\Admin\OrderPoint;
 
+use Modules\Geo\Models\OrderPoint;
 use Tests\TestCase;
 
 class UpdateTest extends TestCase
 {
+    //    public function test_unauthenticated_cannot_update_order_point()
+//    {
+//        //
+//    }
 
+    public function test_authenticated_can_update_order_point()
+    {
+        $orderPoint = OrderPoint::factory()->create();
+
+        $response = $this->json('PATCH', route('admin.order_points.update', $orderPoint), [
+            'name' => $newName = 'new name',
+        ]);
+
+        $response->assertOk();
+        $this->assertDatabaseHas('order_points', [
+            'name' => $newName,
+        ]);
+    }
 }
