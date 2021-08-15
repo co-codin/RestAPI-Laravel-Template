c951c6b08642044ad4fb13a973af5d7ca69751dc
Lenar Khusainov <work@lenarx.ru>
2021-08-15 23:04:56 +0300

    Add more allowed fields

--- Modules/Brand/Repositories/Criteria/BrandRequestCriteria.php
+++ Modules/Brand/Repositories/Criteria/BrandRequestCriteria.php
@@ -14,9 +14,9 @@ class BrandRequestCriteria implements CriteriaInterface
     {
         return QueryBuilder::for($model)
             ->defaultSort('id')
-            ->allowedFields([
-                'id', 'name', 'full_description', 'full_description', 'slug', 'image', 'website', 'country', 'status', 'is_in_home', 'position', 'created_at', 'updated_at', 'deleted_at'
-            ])
+            ->allowedFields(array_merge(
+                static::allowedBrandFields(),
+            ))
             ->allowedFilters([
                 AllowedFilter::exact('id'),
                 AllowedFilter::partial('name'),
@@ -34,5 +34,18 @@ class BrandRequestCriteria implements CriteriaInterface
             ->allowedSorts([
                 'id', 'created_at', 'deleted_at', 'updated_at', 'name', 'slug', 'image', 'website', 'country', 'status', 'in_home', 'position'
             ]);
+    }
+
+    public static function allowedBrandFields($prefix = null): array
+    {
+        $fields = [
+            'id', 'name', 'full_description', 'full_description', 'slug', 'image', 'website', 'country', 'status', 'is_in_home', 'position', 'created_at', 'updated_at', 'deleted_at',
+        ];
+
+        if(!$prefix) {
+            return $fields;
+        }
+
+        return array_map(fn($field) => $prefix . "." . $field, $fields);
     }
 }
