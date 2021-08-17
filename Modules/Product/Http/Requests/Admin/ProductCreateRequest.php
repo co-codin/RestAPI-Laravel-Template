6b624855e9d13cdf541e23eaa7d40d2d92342c2a
Lenar Khusainov <work@lenarx.ru>
2021-08-17 17:26:56 +0300

    Remove all unused fields from product creating. Set default values

--- Modules/Product/Http/Requests/Admin/ProductCreateRequest.php
+++ Modules/Product/Http/Requests/Admin/ProductCreateRequest.php
@@ -27,48 +27,10 @@ class ProductCreateRequest extends BaseFormRequest
             ],
             'categories.*.id' => 'required|integer|distinct|exists:categories,id',
             'categories.*.is_main' => 'required|boolean',
-
-//            'brand_id' => 'required_unless:type,' . Status::ACTIVE,
             'brand_id' => 'required|integer|exists:brands,id',
             'name' => 'required|string|max:255',
             'slug' => 'required|string|max:255|unique:products,slug',
             'image' => 'required|image',
-            'short_description' => 'sometimes|nullable|string',
-            'full_description' => 'sometimes|nullable|string',
-            'warranty' => 'sometimes|nullable|integer',
-            'status' => [
-                'required',
-                'integer',
-                new EnumValue(Status::class, false),
-            ],
-            'is_in_home' => 'required|boolean',
-
-            'documents' => 'sometimes|nullable|array',
-            'documents.*.name' => 'required|string|max:255',
-            'documents.*.source' => [
-                'required',
-                'integer',
-                new EnumValue(DocumentSource::class, false),
-            ],
-
-            'documents.*.file' => [
-                'required_if:documents.*.source,' . DocumentSource::FILE,
-                'file',
-                'exclude_unless:documents.*.source,' . DocumentSource::URL
-            ],
-
-            'documents.*.url' => [
-                'required_if:documents.*.source,' . DocumentSource::URL,
-                'file',
-                'exclude_unless:documents.*.url,' . DocumentSource::FILE
-            ],
-
-            'documents.*.type' => [
-                'required',
-                'integer',
-                new EnumValue(DocumentType::class, false),
-            ],
-            'documents.*.position' => 'sometimes|nullable|integer|distinct',
         ];
     }
 }
