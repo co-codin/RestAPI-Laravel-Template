9914ac338623d57aee177b05f4971aabfff836eb
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-23 16:45:20 +0300

    testing

---
+++ tests/Feature/Modules/Geo/Gql/SoldProductReadTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Modules\Geo\Gql;
+
+use Tests\TestCase;
+
+class SoldProductReadTest extends TestCase
+{
+    public function test_sold_products_can_be_viewed()
+    {
+
+    }
+}
---
+++ tests/Feature/Modules/Geo/Web/SoldProduct/ReadTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Modules\Geo\Web\SoldProduct;
+
+use Tests\TestCase;
+
+class ReadTest extends TestCase
+{
+    public function test_user_can_view_sold_products()
+    {
+
+    }
+}
