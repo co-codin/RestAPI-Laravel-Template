2cb280c1db20266aac4f00cd97f2acb0fac6d37c
Lenar Khusainov <work@lenarx.ru>
2021-09-01 13:55:34 +0300

    Add todo comments for refactoring

--- Modules/Geo/Console/ClientsGeographyImportCommand.php
+++ Modules/Geo/Console/ClientsGeographyImportCommand.php
@@ -19,8 +19,10 @@ use Modules\Product\Models\Product;
  */
 class ClientsGeographyImportCommand extends Command
 {
+    // TODO давай переименуем команду - нельзя в начале команды писать действие. нужно сначала сущность, пример: sold-products:import
     protected $signature = 'import:clients-geography';
 
+    // TODO нужно обновить signature
     protected $description = 'Import clients geography from csv file';
 
     protected Google_Service_Drive $serviceDrive;
@@ -34,6 +36,7 @@ class ClientsGeographyImportCommand extends Command
     )
     {
         parent::__construct();
+        // TODO не в первый раз я уже это пишу - нельзя в constructor логику писать, так как он будет постоянно запускаться при выполнении любой консольной команде
         $this->soldProducts = collect();
         $this->getGoogleServiceDrive();
         $this->getFileContent();
@@ -43,23 +46,29 @@ class ClientsGeographyImportCommand extends Command
     {
         $this->transformCsv();
 
+        // TODO это плохо, так как после каждого запуска будет auto increment у id и совсем скоро наберется куча всего
         SoldProduct::query()->truncate();
 
         foreach ($this->soldProducts as $soldProduct) {
+            // TODO cyrillic в array key - плохой тон
             $cityName = $soldProduct['Город'];
             $city = City::query()
                 ->where([
+                    // TODO почему здесь like? нужно exact search
                     ['name', 'like', "%{$cityName}%"],
                 ])
+                // TODO можно заменить на firstOrCreate
                 ->first();
 
             if (!$city) {
+                // TODO вот тут будет проблема с region_id, который в cities не должен быть nullable
                 $city = City::query()->create([
                     'name' => $cityName,
                 ]);
             }
 
             SoldProduct::query()->create([
+                // TODO title rename to name
                 'title' => $soldProduct['Наименование'],
                 'city_id' => $city->id,
                 'product_id' => $soldProduct['id оборудования'],
@@ -68,6 +77,7 @@ class ClientsGeographyImportCommand extends Command
 
     }
 
+    // TODO проблемы с нейминг - методы с префиксом get должны возвращать что то, а у тебя он не возвращает
     private function getFileContent()
     {
         $response = $this->serviceDrive->files->export(
@@ -80,6 +90,7 @@ class ClientsGeographyImportCommand extends Command
         $response->getBody()->close();
     }
 
+    // TODO у тебя метод transformCsv работает с csv, а потом запускает валидацию, потом mapping. нужно бы разбить на 2 метода
     private function transformCsv()
     {
         $lines = explode("\n", $this->fileContent);
@@ -99,6 +110,7 @@ class ClientsGeographyImportCommand extends Command
         $this->soldProducts = $this->soldProducts
             ->filter(fn($soldProduct) => $this->validateSoldProduct($soldProduct))
             ->map(function ($soldProduct) {
+                // TODO не нужно использовать cyrillic как array key, это очень не надежно
                 $soldProduct['Федеральный округ'] = $this->getDistrict($soldProduct['Федеральный округ']);
                 return $soldProduct;
 //                return collect($soldProduct)->only([
@@ -113,6 +125,9 @@ class ClientsGeographyImportCommand extends Command
 
     private function validateSoldProduct($soldProduct)
     {
+        // TODO валидация сделана очень сложно, нужно сделать как дефолтно в laravel - https://ibb.co/4j4tkNR
+
+
         $nameValidator = array_key_exists('Наименование', $soldProduct) && strlen($soldProduct['Наименование']) < 255;
 
         $districtValidator = array_key_exists('Федеральный округ', $soldProduct);
@@ -130,9 +145,11 @@ class ClientsGeographyImportCommand extends Command
             return [$value->description => $value->value];
         })->toArray();
 
+        // TODO давай не будем central district by default делать. пускай строки с неправильным district будет skip
         return array_key_exists($districtName, $mapped) ? $mapped[$districtName] : District::Central;
     }
 
+    // TODO давай вынесем в отдельный класс, который будет подключаться к google service, а в других классах мы уже будем его в constructor и будем уверены что он со всей авторизацией
     private function getGoogleServiceDrive()
     {
         $client = new Google_Client([
@@ -147,6 +164,7 @@ class ClientsGeographyImportCommand extends Command
         $this->serviceDrive = new Google_Service_Drive(
             $this->googleApiService->getAuthClient(
                 $client,
+                // TODO нужно путь закинуть в storage, у тебя он отличается - сначала storage, потом base
                 base_path('secret-data/google/token_drive.json')
             )
         );
