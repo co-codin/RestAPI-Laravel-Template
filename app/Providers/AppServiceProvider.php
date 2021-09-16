b680e8ccd65433d88e0ad09e6af7b0e1b5c491e2
Lenar Khusainov <work@lenarx.ru>
2021-09-16 18:13:20 +0300

    Fix connecting problems in google drive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
+use Google_Service_Drive;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,15 @@ class AppServiceProvider extends ServiceProvider
             return ClientBuilder::create()
                 ->setHosts($app['config']->get('elasticsearch.hosts'))
                 ->build();
+        });
+
+        $this->app->bind(Google_Service_Drive::class, function ($app) {
+            $client = new \Google_Client();
+            $client->setClientId(config('services.google-api.client_id'));
+            $client->setClientSecret(config('services.google-api.client_secret'));
+            $client->refreshToken(config('services.google-api.refresh_token'));
+
+            return new Google_Service_Drive($client);
         });
     }
 
