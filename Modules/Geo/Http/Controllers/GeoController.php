ebb5aeb747cf9ebbe3c199970677d01f609a7031
ElijahCoding <cuiyeqing960904@gmail.com>
2021-09-16 20:21:15 +0300

    geo city detection built

--- Modules/Geo/Http/Controllers/GeoController.php
+++ Modules/Geo/Http/Controllers/GeoController.php
@@ -9,15 +9,11 @@ use Modules\Geo\Services\SxGeo;
 
 class GeoController extends Controller
 {
-    public function detectIp(Request $request)
+    public function detectCity(Request $request)
     {
-        $request->validate([
-            'ip' => 'required|ip'
-        ]);
-
         $SxGeo = new SxGeo(storage_path('app/SxGeoCity.dat', SXGEO_BATCH | SXGEO_MEMORY));
 
-        $city = $SxGeo->getCity($request->get('ip'));
+        $city = $SxGeo->getCity($request->ip());
 
         $defaultCity = City::query()->where('is_default', '=', 1)->first();
 
