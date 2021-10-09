0d7baa881534a8c971a606cdee326e04595e22e7
ElijahCoding <cuiyeqing960904@gmail.com>
2021-10-09 18:03:57 +0300

    building category builder

---
+++ Modules/Category/Services/CategoryBuilder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Category\Services;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class CategoryBuilder
+{
+    public function rootCategory(Builder $builder, array $productIds): Builder
+    {
+
+
+    }
+}
