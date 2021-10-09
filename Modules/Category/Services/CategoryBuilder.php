3fe6eb4b47ee97fe475cb8be2e4516c82de52bb4
ElijahCoding <cuiyeqing960904@gmail.com>
2021-10-09 19:19:25 +0300

    building getRootCategories builder

--- Modules/Category/Services/CategoryBuilder.php
+++ Modules/Category/Services/CategoryBuilder.php
@@ -3,12 +3,29 @@
 namespace Modules\Category\Services;
 
 use Illuminate\Database\Eloquent\Builder;
+use Modules\Category\Models\Category;
+
 
 class CategoryBuilder
 {
-    public function rootCategory(Builder $builder, array $productIds): Builder
+    public function getRootCategories(Builder $builder, array $productIds)
     {
-
-
+        dd(
+            $builder->first()->toArray()
+        );
+//        $parentCategories = Category::query()->whereHas(
+//            'productCategories',
+//            fn($builder) => $builder->whereIn('product_id', $productIds)
+//        )->with(['parent'])->get()->pluck('parent');
+//
+//        $parentCategories->each(function(Category $parentCategory) use ($productIds) {
+//            $parentCategory->descendants()->each(function (Category $category) use ($productIds, $parentCategory) {
+//                $count = $category->productCategories()->whereIn('product_id', $productIds)->count();
+//                $parentCategory->product_count += $count;
+//            });
+//            $parentCategory->product_count += $parentCategory->productCategories()->whereIn('product_id', $productIds)->count();
+//        });
+//
+//        return $parentCategories;
     }
 }
