525b1b807dd92bfdb0450df68a8cdca618258ec9
ElijahCoding <cuiyeqing960904@gmail.com>
2021-10-09 19:23:52 +0300

    fixing parent

--- Modules/Category/Services/CategoryBuilder.php
+++ Modules/Category/Services/CategoryBuilder.php
@@ -10,22 +10,16 @@ class CategoryBuilder
 {
     public function getRootCategories(Builder $builder, array $productIds)
     {
-        dd(
-            $builder->first()->toArray()
-        );
-//        $parentCategories = Category::query()->whereHas(
-//            'productCategories',
-//            fn($builder) => $builder->whereIn('product_id', $productIds)
-//        )->with(['parent'])->get()->pluck('parent');
-//
-//        $parentCategories->each(function(Category $parentCategory) use ($productIds) {
-//            $parentCategory->descendants()->each(function (Category $category) use ($productIds, $parentCategory) {
-//                $count = $category->productCategories()->whereIn('product_id', $productIds)->count();
-//                $parentCategory->product_count += $count;
-//            });
-//            $parentCategory->product_count += $parentCategory->productCategories()->whereIn('product_id', $productIds)->count();
-//        });
-//
-//        return $parentCategories;
+        return $builder->whereHas(
+            'productCategories',
+            fn($builder) => $builder->whereIn('product_id', $productIds)
+        )->with('parent')->pluck('parent')
+            ->each(function (Category $parentCategory) use ($productIds) {
+            $parentCategory->descendants()->each(function (Category $category) use ($productIds, $parentCategory) {
+                $count = $category->productCategories()->whereIn('product_id', $productIds)->count();
+                $parentCategory->product_count += $count;
+            });
+            $parentCategory->product_count += $parentCategory->productCategories()->whereIn('product_id', $productIds)->count();
+        });
     }
 }
