dd64a71d03ee7ad8dc6aa21054ee58255f476440
ElijahCoding <cuiyeqing960904@gmail.com>
2021-10-10 13:05:43 +0300

    covid products

--- Modules/Product/Services/ProductBuilders.php
+++ Modules/Product/Services/ProductBuilders.php
@@ -5,10 +5,13 @@ namespace Modules\Product\Services;
 
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 use Modules\Category\Models\Category;
 
 class ProductBuilders
 {
+    const COVID_PROPERTY_ID = 259;
+
     public function rootCategory(Builder $builder, int $rootCategory): Builder
     {
         $rootCategory = Category::findOrFail($rootCategory);
@@ -17,5 +20,16 @@ class ProductBuilders
             'productCategories',
             fn($builder) => $builder->whereIn('category_id', $rootCategory->descendants()->pluck('id'))
         );
+    }
+
+    public function fromCovid(Builder $builder, bool $fromCovid): Builder
+    {
+        return $builder->whereExists(function($query) use ($fromCovid) {
+            $query->select(DB::raw(1))
+                ->from('product_property as pp')
+                ->whereColumn('pp.product_id', 'products.id')
+                ->where('pp.property_id', static::COVID_PROPERTY_ID)
+                ->whereJsonContains('pp.value', true);
+        });
     }
 }
