bb4f9c47dcfbf62e17414de0a6d7ff3829d522b9
ElijahCoding <cuiyeqing960904@gmail.com>
2021-10-10 14:53:22 +0300

    refactoring home data

---
+++ app/Services/HomeBuilder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class HomeBuilder
+{
+    public function getHomeData($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+
+    }
+}
