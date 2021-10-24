f0a275d088764d5526014cfb5d7f8a491bcfb741
ElijahCoding <cuiyeqing960904@gmail.com>
2021-10-24 23:03:38 +0300

    type lang added

---
+++ Modules/Filter/Resources/lang/ru/enums.php
@@ -0,0 +1,11 @@
+<?php
+
+use Modules\Filter\Enums\FilterType;
+
+return [
+    FilterType::class => [
+        FilterType::CheckMarkList => 'Хранилище',
+        FilterType::Slider => 'Шаг',
+        FilterType::CheckMark => 'Значение для поиска',
+    ],
+];
