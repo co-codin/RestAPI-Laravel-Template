f28378e1e0710bfad54b8665a817ea502b65da96
Lenar Khusainov <work@lenarx.ru>
2021-10-24 09:21:38 +0300

    Add command description

--- Modules/Export/Services/ExportScheduler.php
+++ Modules/Export/Services/ExportScheduler.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Database\Eloquent\Collection;
 use Modules\Export\Console\ExportFeedCommand;
 use Modules\Export\Enum\ExportFrequency;
+use Modules\Export\Enum\ExportType;
 use Modules\Export\Models\Export;
 
 class ExportScheduler
@@ -14,7 +15,9 @@ class ExportScheduler
     {
         foreach ($this->getAllScheduledExports() as $export) {
             $frequency = ExportFrequency::getFrequency($export->frequency);
-            $schedule->command(ExportFeedCommand::class, [$export->id])->{$frequency}();
+            $schedule->command(ExportFeedCommand::class, [$export->id])
+                ->description("Экспорт товаров для " . ExportType::getDescription($export->type))
+                ->{$frequency}();
         }
     }
 
