dd18c2049094bee63551b3fbcd53a2ea5954d792
Lenar Khusainov <work@lenarx.ru>
2021-10-28 11:43:41 +0300

    Add satom feed generator

---
+++ Modules/Export/Services/Generators/Satom/Entities/CategoryGenerator.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Modules\Export\Services\Generators\Satom\Entities;
+
+use App\Enums\Status;
+use Bukashk0zzz\YmlGenerator\Model\Category as CategoryYaml;
+use Illuminate\Support\Collection;
+use Modules\Category\Models\Category;
+
+
+class CategoryGenerator
+{
+    public function getCategories(): array
+    {
+        $categories = [];
+        $categoryEntities = $this->getCategoryEntities();
+
+        foreach ($categoryEntities as $category) {
+            $categories[] = (new CategoryYaml())
+                ->setId($category->id)
+                ->setParentId($category->parent_id)
+                ->setName($category->name);
+        }
+
+        return $categories;
+    }
+
+    private function getCategoryEntities(): Collection
+    {
+        return Category::query()
+            ->defaultOrder()
+            ->whereStatus(Status::ACTIVE)
+            ->get();
+    }
+}
