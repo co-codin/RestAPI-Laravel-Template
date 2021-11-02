262cb4be97de39edc7fb0edd6dde26fba4739456
Lenar Khusainov <work@lenarx.ru>
2021-11-02 12:26:27 +0300

    Add formatted description field

--- Modules/News/Models/News.php
+++ Modules/News/Models/News.php
@@ -86,6 +86,32 @@ class News extends Model
         return $query->whereIn('status', [Status::ACTIVE, Status::ONLY_URL]);
     }
 
+    public function getFormattedFullDescriptionAttribute()
+    {
+        $re = '/'
+            . '(href=")'
+            . '('
+            . '(?!(https:\/\/|http:\/\/)?medeqstars\.ru|(https:\/\/|http:\/\/)?medeq\.ru)'
+            . '(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?'
+            . '(?:'
+            . '(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})'
+            . '(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})'
+            . '(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}'
+            . '(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))'
+            . '|'
+            . '(?:(?:[a-z-0-9\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)'
+            . '(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z-0-9\x{00a1}-\x{ffff}]{2,}))'
+            . ')'
+            . '(?::\d{2,5})?(?:\/[^\s]*)?"'
+            . '(?<!medeqstars\.ru|medeq\.ru)'
+            . ')'
+            . '/iu';
+
+        return preg_replace_callback($re, function ($matches) {
+            return $matches[1] . '/go?to=' . $matches[2] . ' target="_blank"';
+        }, $this->full_description);
+    }
+
     protected static function newFactory()
     {
         return NewsFactory::new();
