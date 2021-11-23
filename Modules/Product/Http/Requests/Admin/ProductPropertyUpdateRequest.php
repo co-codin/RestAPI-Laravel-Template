01041e5f27babf57b8b00878e908d208a4f37796
Lenar Khusainov <work@lenarx.ru>
2021-11-23 14:52:09 +0300

    Fix properties validation

--- Modules/Product/Http/Requests/Admin/ProductPropertyUpdateRequest.php
+++ Modules/Product/Http/Requests/Admin/ProductPropertyUpdateRequest.php
@@ -13,12 +13,12 @@ class ProductPropertyUpdateRequest extends BaseFormRequest
         return [
             'properties' => 'nullable|array',
             'properties.*.id' => 'required|distinct|integer|exists:properties,id',
-            'properties.*.field_value_ids' => 'sometimes|nullable|exists:field_values,id',
-            'properties.*.pretty_key' => 'sometimes|nullable|string|max:255',
-            'properties.*.pretty_value' => 'sometimes|nullable|string|max:255',
-            'properties.*.is_important' => 'sometimes|boolean',
-            'properties.*.important_position' => 'sometimes|nullable|integer',
-            'properties.*.important_value' => 'sometimes|required_if:properties.*.is_important,true|nullable|string|max:255',
+            'properties.*.field_value_ids' => 'nullable|exists:field_values,id',
+            'properties.*.pretty_key' => 'nullable|string|max:255',
+            'properties.*.pretty_value' => 'nullable|string|max:255',
+            'properties.*.is_important' => 'boolean',
+            'properties.*.important_position' => 'nullable|integer',
+            'properties.*.important_value' => 'required_if:properties.*.is_important,true|nullable|string|max:255',
         ];
     }
 }
