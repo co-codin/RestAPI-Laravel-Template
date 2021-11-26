f956b24b0670149b18214b685e820915eb1a80a9
dmitry <d.bormisov@medeqstars.ru>
2021-11-26 14:00:37 +0300

    MQ-2519 fix

--- Modules/Brand/Services/BrandBuilder.php
+++ Modules/Brand/Services/BrandBuilder.php
@@ -9,13 +9,14 @@ class BrandBuilder
 {
     public function getByCategories(Builder $builder, array $categoryIds): Builder
     {
-        return $builder->joinSub(function (\Illuminate\Database\Query\Builder $query) use ($categoryIds) {
-            $query
-                ->select('p.brand_id')
-                ->from('products as p')
-                ->join('product_category as pc', 'pc.product_id', '=', 'p.id')
-                ->whereIn('pc.category_id', $categoryIds)
-                ->groupBy('p.brand_id');
-        }, 'grouped_brands', 'grouped_brands.brand_id', '=', 'brands.id');
+        return $builder
+            ->whereExists(function (\Illuminate\Database\Query\Builder $query) use ($categoryIds) {
+                $query
+                    ->select(\DB::raw(1))
+                    ->from('products as p')
+                    ->join('product_category as pc', 'pc.product_id', '=', 'p.id')
+                    ->whereIn('pc.category_id', $categoryIds)
+                    ->whereRaw('p.brand_id = brands.id');
+            });
     }
 }
