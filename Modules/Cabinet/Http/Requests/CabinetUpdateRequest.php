fc69d3f42e03b9165c459a21a7144d6a740c2714
ElijahCoding <cuiyeqing960904@gmail.com>
2021-11-30 16:42:16 +0300

    building create + update + destroy

---
+++ Modules/Cabinet/Http/Requests/CabinetUpdateRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Cabinet\Http\Requests;
+
+use App\Enums\Status;
+use App\Http\Requests\BaseFormRequest;
+use BenSampo\Enum\Rules\EnumValue;
+
+class CabinetUpdateRequest extends BaseFormRequest
+{
+    public function rules()
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'slug' => 'sometimes|nullable|max:255|regex:/^[a-z0-9_\-]*$/|unique:cabinets,slug',
+            'is_image_changed' => 'sometimes|boolean',
+            'image' => 'sometimes|exclude_unless:is_image_changed,true|nullable|image',
+            'category_id' => 'sometimes|required|integer|exists:categories,id',
+            'full_description' => 'sometimes|required|string',
+            'status' => [
+                'sometimes',
+                'required',
+                new EnumValue(Status::class, false),
+            ],
+        ];
+    }
+}
--- Modules/Cabinet/Services/CabinetStorage.php
+++ Modules/Cabinet/Services/CabinetStorage.php
@@ -2,7 +2,24 @@
 
 namespace Modules\Cabinet\Services;
 
+use App\Services\File\ImageUploader;
+
 class CabinetStorage
 {
+    public function __construct(protected ImageUploader $imageUploader) {}
+    
+    public function store()
+    {
 
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
