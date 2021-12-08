e701a3c6b138027c2ea1a27b868d0c44e52e11ff
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-08 17:10:57 +0300

    page seo

--- Modules/Page/Routes/admin.php
+++ Modules/Page/Routes/admin.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Page\Http\Controllers\Admin\PageController;
+use Modules\Page\Http\Controllers\Admin\PageSeoController;
+
+Route::patch('pages/{page}/seo', [PageSeoController::class, 'update']);
 
 Route::resource('pages', PageController::class)->except(['index', 'show']);
