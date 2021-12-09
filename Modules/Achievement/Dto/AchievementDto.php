f8629d4cfa6736466075417debc59aec88aff22f
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-09 15:10:45 +0300

    updated

--- Modules/Achievement/Dto/AchievementDto.php
+++ Modules/Achievement/Dto/AchievementDto.php
@@ -9,9 +9,7 @@ use Illuminate\Http\UploadedFile;
 class AchievementDto extends BaseDto
 {
     public ?string $name;
-
-    public $is_image_changed;
-
+    
     public ?UploadedFile $image;
 
     public mixed $is_enabled = 1;
