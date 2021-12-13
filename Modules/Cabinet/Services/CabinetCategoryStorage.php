4dccf7728fbba2b30028dae49ae7a0e82b329b64
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-14 02:15:56 +0300

    fixed

--- Modules/Cabinet/Services/CabinetCategoryStorage.php
+++ Modules/Cabinet/Services/CabinetCategoryStorage.php
@@ -3,37 +3,21 @@
 namespace Modules\Cabinet\Services;
 
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\DB;
 use Modules\Cabinet\Models\Cabinet;
-use Modules\Category\Models\Category;
 
 class CabinetCategoryStorage
 {
     public function update(Cabinet $cabinet, array $categories)
     {
-        $categories = collect($categories);
-
-        DB::beginTransaction();
-
-        $ids = $categories->pluck('id')->filter()->unique();
-
-        $cabinet->categories()
-            ->when($ids->isNotEmpty(), fn($query) => $query->whereNotIn('id', $ids))
-            ->delete();
-
-        $newCategories = $categories->filter(fn($item) => !Arr::exists($item, 'id'));
-
-        $cabinet->categories()->createMany($newCategories);
-
-        $categories
-            ->filter(fn($category) => Arr::exists($category, 'id'))
-            ->each(function($category) {
-                $model = Category::find($category['id']);
-                if($model) {
-                    $model->update($category);
-                }
-            });
-
-        DB::commit();
+        $cabinet->categories()->sync(
+            collect($categories)
+                ->keyBy('id')
+                ->map(fn($item) => Arr::only($item, [
+                    'name',
+                    'price',
+                    'count',
+                ]))
+                ->toArray()
+        );
     }
 }
