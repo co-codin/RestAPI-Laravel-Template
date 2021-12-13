106d7fa082e7d2d354abed3ff08ce86e9eeea977
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-14 02:31:16 +0300

    added

--- Modules/Cabinet/Services/CabinetDocumentStorage.php
+++ Modules/Cabinet/Services/CabinetDocumentStorage.php
@@ -8,6 +8,14 @@ class CabinetDocumentStorage
 {
     public function update(Cabinet $cabinet, array $documents)
     {
-
+        $cabinet->documents()->delete();
+            foreach ($documents as $document) {
+                $cabinet->documents()->create([
+                    'name' => $document['name'],
+                    'group_name' => $document['group_name'],
+                    'type' => $document['type'],
+                    'file' => $document['file'],
+                ]);
+            }
     }
 }
