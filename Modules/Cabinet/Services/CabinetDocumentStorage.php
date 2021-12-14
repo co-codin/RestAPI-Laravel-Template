b93d2a659b0160162ef5cda801f039b101edea6d
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-14 18:14:25 +0300

    bug fixed

--- Modules/Cabinet/Services/CabinetDocumentStorage.php
+++ Modules/Cabinet/Services/CabinetDocumentStorage.php
@@ -2,14 +2,20 @@
 
 namespace Modules\Cabinet\Services;
 
+use App\Services\File\FileUploader;
 use Modules\Cabinet\Models\Cabinet;
 
 class CabinetDocumentStorage
 {
+    public function __construct(
+        protected FileUploader $fileUploader
+    ) {}
+
     public function update(Cabinet $cabinet, array $documents)
     {
         $cabinet->documents()->delete();
             foreach ($documents as $document) {
+                $document['file'] = $document['file'] ? $this->fileUploader->upload($document['file']) : null;
                 $cabinet->documents()->create([
                     'name' => $document['name'],
                     'group_name' => $document['group_name'],
