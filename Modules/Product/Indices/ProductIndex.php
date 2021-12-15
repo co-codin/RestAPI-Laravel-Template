36e3a1b5582e9962d4dca628a8ff9644040fcf69
Lenar Khusainov <work@lenarx.ru>
2021-12-15 08:32:43 +0300

    Move sort value to main

--- Modules/Product/Indices/ProductIndex.php
+++ Modules/Product/Indices/ProductIndex.php
@@ -414,6 +414,9 @@ class ProductIndex extends BaseIndex
                 'popular_score' => [
                     'type' => 'byte',
                 ],
+                'availability_sort_value' => [
+                    'type' => 'byte',
+                ],
                 'brand' => [
                     'properties' => [
                         'name' => [
@@ -514,9 +517,6 @@ class ProductIndex extends BaseIndex
                                 ],
                             ],
                         ],
-                    ],
-                    'availability_sort_value' => [
-                        'type' => 'byte',
                     ],
                 ],
                 'properties' => [
