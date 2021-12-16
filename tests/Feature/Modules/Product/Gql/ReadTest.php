065b450167e250792c0a6f21dc9d585fe2d355d3
dmitry <d.bormisov@medeqstars.ru>
2021-12-16 16:40:52 +0300

    MQ-2656 ad graphql tests

--- tests/Feature/Modules/Product/Gql/ReadTest.php
+++ tests/Feature/Modules/Product/Gql/ReadTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Modules\Product\Gql;
 
 use Modules\Product\Models\Product;
+use Modules\Product\Models\ProductAnswer;
+use Modules\Product\Models\ProductQuestion;
 use Tests\TestCase;
 
 class ReadTest extends TestCase
@@ -66,6 +68,153 @@ class ReadTest extends TestCase
                         [
                             'id' => $product->id,
                             'name' => $product->name,
+                        ]
+                    ],
+                ]
+            ],
+        ]);
+    }
+
+    public function test_product_questions_can_be_viewed(): void
+    {
+        $productQuestion = ProductQuestion::factory()->create();
+
+        $response = $this->graphQL('
+            {
+                product_questions {
+                    data {
+                        id
+                        product_id
+                        client_id
+                        status
+                        text
+                        product {
+                            id
+                            name
+                        }
+                    }
+                    paginatorInfo {
+                        currentPage
+                        lastPage
+                    }
+                }
+            }
+        ');
+
+        $response->assertJson([
+            'data' => [
+                'product_questions' => [
+                    'data' => [
+                        [
+                            'id' => $productQuestion->id,
+                            'product_id' => $productQuestion->product_id,
+                            'client_id' => $productQuestion->client_id,
+                            'status' => $productQuestion->status,
+                            'text' => $productQuestion->text,
+                        ]
+                    ],
+                    'paginatorInfo' => [
+                        'currentPage' => 1,
+                        'lastPage' => 1,
+                    ]
+                ]
+            ],
+        ]);
+
+        $response = $this->graphQL('
+            {
+                product_questions(where: { column: ID, operator: EQ, value: ' . $productQuestion->id .'  }) {
+                    data {
+                        id
+                        product_id
+                        client_id
+                        status
+                    }
+                }
+            }
+        ');
+
+        $response->assertJson([
+            'data' => [
+                'product_questions' => [
+                    'data' => [
+                        [
+                            'id' => $productQuestion->id,
+                            'name' => $productQuestion->name,
+                            'product_id' => $productQuestion->product_id,
+                            'client_id' => $productQuestion->client_id,
+                            'status' => $productQuestion->status,
+                        ]
+                    ],
+                ]
+            ],
+        ]);
+    }
+
+    public function test_product_answers_can_be_viewed(): void
+    {
+        $productAnswer = ProductAnswer::factory()->create();
+
+        $response = $this->graphQL('
+            {
+                product_answers {
+                    data {
+                        id
+                        product_question_id
+                        text
+                        first_name
+                        last_name
+                        productQuestion {
+                            id
+                            product_id
+                        }
+                    }
+                    paginatorInfo {
+                        currentPage
+                        lastPage
+                    }
+                }
+            }
+        ');
+
+        $response->assertJson([
+            'data' => [
+                'product_answers' => [
+                    'data' => [
+                        [
+                            'id' => $productAnswer->id,
+                            'product_question_id' => $productAnswer->product_question_id,
+                            'text' => $productAnswer->text,
+                            'first_name' => $productAnswer->first_name,
+                            'last_name' => $productAnswer->last_name,
+                        ]
+                    ],
+                    'paginatorInfo' => [
+                        'currentPage' => 1,
+                        'lastPage' => 1,
+                    ]
+                ]
+            ],
+        ]);
+
+        $response = $this->graphQL('
+            {
+                product_answers(where: { column: ID, operator: EQ, value: ' . $productAnswer->id .'  }) {
+                    data {
+                        id
+                        product_question_id
+                    }
+                }
+            }
+        ');
+
+        $response->assertJson([
+            'data' => [
+                'product_answers' => [
+                    'data' => [
+                        [
+                            'id' => $productAnswer->id,
+                            'product_question_id' => $productAnswer->product_question_id,
                         ]
                     ],
                 ]
