0cf10a2ae2fbaf3f9876c8db8aaf9e708eff91fe
dmitry <d.bormisov@medeqstars.ru>
2021-12-20 16:24:01 +0300

    fix route

--- Modules/Review/Routes/admin.php
+++ Modules/Review/Routes/admin.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Review\Http\Controllers\Admin\ProductReviewController;
 
-Route::apiResource('product-reviews', ProductReviewController::class)->except(['index', 'show', 'store']);
+Route::apiResource('product-reviews', ProductReviewController::class)->except(['index', 'show']);
 
 Route::put('product-reviews/{product_review}/approve', [ProductReviewController::class, 'approve'])->name('product-reviews.approve');
 Route::put('product-reviews/{product_review}/reject', [ProductReviewController::class, 'reject'])->name('product-reviews.reject');
