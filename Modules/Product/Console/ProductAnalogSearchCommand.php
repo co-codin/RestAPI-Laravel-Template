fdeb9a07eba44155c8323cd69a4f8e2342c2c53d
dmitry <d.bormisov@medeqstars.ru>
2021-12-24 11:55:35 +0300

    MQ-2668 change query

--- Modules/Product/Console/ProductAnalogSearchCommand.php
+++ Modules/Product/Console/ProductAnalogSearchCommand.php
@@ -4,10 +4,13 @@ namespace Modules\Product\Console;
 
 use App\Enums\Status;
 use Illuminate\Console\Command;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection as SupportCollection;
 
 class ProductAnalogSearchCommand extends Command
 {
+    const COUNT_ANALOGS = 12;
+
     protected $signature = 'search:product-analogs';
 
     protected $description = 'Search product analogs';
@@ -33,21 +36,29 @@ class ProductAnalogSearchCommand extends Command
                         ->join('products as p', 'p.id', '=', 'pp.product_id')
                         ->where('pp.product_id', '!=', $productId)
                         ->where('p.status', Status::ACTIVE)
-                        ->where('p.is_manually_analogs', false);
+                        ->where('p.is_manually_analogs', false)
+                        ->where(function (Builder $query) use ($productProperties) {
+                            foreach ($productProperties as $key => $property) {
+                                $where = !$key ? 'where' : 'orWhere';
 
-                    foreach ($productProperties as $property) {
-                        $analogs = $analogs
-                            ->orWhere(function ($query) use ($property) {
-                                $query
-                                    ->where('pp.property_id', $property->property_id)
-                                    ->where('pp.field_value_ids', $property->field_value_ids);
-                            });
-                    }
+                                $query->{$where}(function (Builder $query) use ($property) {
+                                    $query->where('pp.property_id', $property->property_id);
 
-                    $analogs = $analogs
+                                    $fieldValueIds = json_decode($property->field_value_ids, false, 512, JSON_THROW_ON_ERROR);
+
+                                    if (is_array($fieldValueIds)) {
+                                        foreach ($fieldValueIds as $fieldValueId) {
+                                            $query->whereRaw("JSON_CONTAINS(JSON_EXTRACT(pp.field_value_ids, '$[*]'), '$fieldValueId', '$')");
+                                        }
+                                    } else {
+                                        $query->where('pp.field_value_ids', $fieldValueIds);
+                                    }
+                                });
+                            }
+                        })
                         ->groupBy('analog_id')
                         ->orderByDesc('total')
-                        ->limit(12)
+                        ->limit(self::COUNT_ANALOGS)
                         ->get();
 
                     $analogsData = $analogs->map(function (object $analogData) use ($productId) {
