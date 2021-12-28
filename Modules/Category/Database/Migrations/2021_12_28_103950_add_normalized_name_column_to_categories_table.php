89b9155aed802f09d132c47dc0e1736913eebdb2
Lenar Khusainov <work@lenarx.ru>
2021-12-28 10:47:14 +0300

    Add category name to products filter

---
+++ Modules/Category/Database/Migrations/2021_12_28_103950_add_normalized_name_column_to_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNormalizedNameColumnToCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('name_normalized')->after('name')->virtualAs("regexp_replace(name, '[^A-ZА-Яа-яa-z0-9]', '')")->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('name_normalized');
+        });
+    }
+}
