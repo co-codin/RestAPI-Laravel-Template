45ddb36211b08145355d68a2ef6ce235c990161d
Lenar Khusainov <work@lenarx.ru>
2021-12-28 09:46:05 +0300

    Add multi search to live filter

--- Modules/Product/Http/Filters/ProductLiveFilter.php
+++ Modules/Product/Http/Filters/ProductLiveFilter.php
@@ -14,13 +14,13 @@ class ProductLiveFilter implements Filter
         collect(str_getcsv($value, ' ', '"'))
             ->filter()
             ->each(function ($term) use ($query) {
-                $term = preg_replace('/[^A-ZА-Яа-яa-z0-9]/', '', $term).'%';
+                $term = '%'.preg_replace('/[^A-ZА-Яа-яa-z0-9]/', '', $term).'%';
                 $query->whereIn('id', function ($query) use ($term) {
                     $query->select('id')
                         ->from(function ($query) use ($term) {
                             $query->select('products.id')
                                 ->from('products')
-                                ->where('products.model', 'like', $term)
+                                ->where('products.name', 'like', $term)
                                 ->union(
                                     $query->newQuery()
                                         ->select('products.id')
