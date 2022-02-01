f43a69f5d01cabd202516a2c3184de5ea229fb3c
ElijahCoding <cuiyeqing960904@gmail.com>
2022-02-01 14:26:14 +0300

    fixed

---
+++ Modules/Vacancy/Database/Migrations/2022_02_01_142358_change_short_description_type_in_vacancies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeShortDescriptionTypeInVacanciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+            $table->text('short_description')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
