3ac29ad627d0e5314dd1bf7aea9ae69487413bd6
ElijahCoding <cuiyeqing960904@gmail.com>
2022-02-04 16:03:44 +0300

    built

--- Modules/Brand/Services/BrandBuilder.php
+++ Modules/Brand/Services/BrandBuilder.php
@@ -18,15 +18,6 @@ class BrandBuilder
                     ->leftJoin('brands as b', 'p.brand_id', '=', 'b.id')
                     ->whereIn('pc.category_id', $categoryIds)
                     ;
-
-//
-//                    $query
-//                        ->select(\DB::raw(1))
-//                        ->from('products as p')
-//                        ->join('product_category as pc', 'pc.product_id', '=', 'p.id')
-//                        ->join('categories as c', 'c.parent_id', '=', 'pc.category_id')
-//                        ->whereIn('pc.category_id', $categoryIds)
-//                        ->whereRaw('p.brand_id = brands.id');
             });
     }
 }
