c3c2b902715d1600d109d52e8f03c9bb1da11338
ElijahCoding <cuiyeqing960904@gmail.com>
2022-02-11 14:39:21 +0300

    fixing

--- Modules/Brand/Services/BrandBuilder.php
+++ Modules/Brand/Services/BrandBuilder.php
@@ -12,14 +12,22 @@ class BrandBuilder
     {
         return $builder
             ->whereExists(function (\Illuminate\Database\Query\Builder $query) use ($categoryIds) {
-                $query
-                    ->select(\DB::raw(1))
-                    ->from('products as p')
-                    ->join('product_category as pc', 'pc.product_id', '=', 'p.id')
-                    ->join('categories as c', 'c.parent_id', '=', 'pc.category_id')
+                $query->select('*')
+                    ->from('product_category as pc')
+                    ->leftJoin('products as p', 'pc.product_id', '=', 'p.id')
+                    ->leftJoin('brands as b', 'p.brand_id', '=', 'b.id')
                     ->whereIn('pc.category_id', $categoryIds)
+                    ->where('p.status', '=', Status::ACTIVE)
                     ->whereRaw('p.brand_id = brands.id');
 
+//
+//                $query
+//                    ->select(\DB::raw(1))
+//                    ->from('products as p')
+//                    ->join('product_category as pc', 'pc.product_id', '=', 'p.id')
+//                    ->join('categories as c', 'c.parent_id', '=', 'pc.category_id')
+//                    ->whereIn('pc.category_id', $categoryIds)
+//                    ->whereRaw('p.brand_id = brands.id');
             });
     }
 }
