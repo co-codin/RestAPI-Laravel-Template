01d81f0d81c6af1087c27b24317fef91e38feec3
ElijahCoding <cuiyeqing960904@gmail.com>
2022-02-14 17:27:35 +0300

    testing

---
+++ tests/Feature/Modules/Product/Admin/ProductVariationProperty/UpdateTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Modules\Product\Admin\ProductVariationProperty;
+
+use Tests\TestCase;
+
+class UpdateTest extends TestCase
+{
+    public function test_authenticated_can_update_product_variation_property()
+    {
+
+    }
+}
