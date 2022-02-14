b147391cc8f168127198282ca7d6e281831294e9
ElijahCoding <cuiyeqing960904@gmail.com>
2022-02-14 17:03:06 +0300

    testing api

--- Modules/Product/Services/ProductVariationPropertyStorage.php
+++ Modules/Product/Services/ProductVariationPropertyStorage.php
@@ -2,10 +2,12 @@
 
 namespace Modules\Product\Services;
 
+use Modules\Product\Models\ProductVariationProperty;
+
 class ProductVariationPropertyStorage
 {
     public function update(array $data)
     {
-        
+        ProductVariationProperty::query()->create($data);
     }
 }
