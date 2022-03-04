321dd2f8a5c47b387db11949daeeda64b77db95c
elijah <cuiyeqing960904@gmail.com>
2022-03-04 11:55:09 +0000

    Revert "Merge branch 'master-fix' into 'master'"
    This reverts merge request !450

--- Modules/Achievement/Dto/AchievementDto.php
+++ Modules/Achievement/Dto/AchievementDto.php
@@ -4,12 +4,15 @@
 namespace Modules\Achievement\Dto;
 
 use App\Dto\BaseDto;
+use Illuminate\Http\UploadedFile;
 
 class AchievementDto extends BaseDto
 {
     public ?string $name;
 
-    public ?string $image;
+    public $is_image_changed;
+
+    public ?UploadedFile $image;
 
     public mixed $is_enabled = 1;
 
