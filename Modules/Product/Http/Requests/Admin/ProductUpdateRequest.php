1c76343ea69ac82e9e8eb012394c38531bd026bb
Lenar Khusainov <work@lenarx.ru>
2022-03-05 13:00:03 +0300

    Add name attribute translation

--- Modules/Product/Http/Requests/Admin/ProductUpdateRequest.php
+++ Modules/Product/Http/Requests/Admin/ProductUpdateRequest.php
@@ -135,6 +135,7 @@ class ProductUpdateRequest extends BaseFormRequest
             'documents.*.docs.*.type' => 'Тип',
             'documents.*.docs.*.file' => 'Файл',
             'documents.*.docs.*.link' => 'Ссылка',
+            'name' => 'Модель',
         ];
     }
 }
