c7336e646a7a24c1abf01b5340da3b79a2de3aba
ElijahCoding <cuiyeqing960904@gmail.com>
2022-04-04 16:50:13 +0300

    achievement tests fixed

--- tests/Feature/Modules/Achievement/Admin/DeleteTest.php
+++ tests/Feature/Modules/Achievement/Admin/DeleteTest.php
@@ -9,14 +9,18 @@ use Tests\TestCase;
 
 class DeleteTest extends TestCase
 {
-//    public function test_unauthenticated_cannot_delete_achievement()
-//    {
-//        //
-//    }
+    public function test_unauthenticated_cannot_delete_achievement()
+    {
+        $achievement = Achievement::factory()->create();
+
+        $response = $this->deleteJson(route('admin.achievements.destroy', $achievement));
+
+        $response->assertStatus(401);
+    }
 
     public function test_authenticated_can_delete_achievement()
     {
-        $this->withoutExceptionHandling();
+        $this->authenticateUser();
 
         $achievement = Achievement::factory()->create();
 
