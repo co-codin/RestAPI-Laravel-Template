ea1662b0f6970f553f62bc466bbdc8afe15cfa48
ElijahCoding <cuiyeqing960904@gmail.com>
2022-04-04 15:40:59 +0300

    user test fixedg

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,1 @@
 <?php
-
-use App\Http\Controllers\AuthController;
-
-Route::get('/login', [AuthController::class, 'loginPage'])->name('web.login');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
