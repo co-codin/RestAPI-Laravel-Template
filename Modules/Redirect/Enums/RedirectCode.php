fac860b2eedf38f1399479944f9ee8384dfbcd7d
Lenar Khusainov <work@lenarx.ru>
2022-04-17 10:13:54 +0300

    Add redirect enum

---
+++ Modules/Redirect/Enums/RedirectCode.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Modules\Redirect\Enums;
+
+
+use App\Enums\BaseEnum;
+
+class RedirectCode extends BaseEnum
+{
+    const REDIRECT_301 = 301;
+
+    const REDIRECT_302 = 302;
+}
