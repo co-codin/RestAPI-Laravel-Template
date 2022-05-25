0853066601b214d65d31bb5292716e4cb5456114
ElijahCoding <cuiyeqing960904@gmail.com>
2022-05-26 00:25:51 +0300

    requests

--- Modules/Contact/Http/Requests/ContactCreateRequest.php
+++ Modules/Contact/Http/Requests/ContactCreateRequest.php
@@ -2,7 +2,21 @@
 
 namespace Modules\Contact\Http\Requests;
 
-class ContactCreateRequest
-{
+use App\Http\Requests\BaseFormRequest;
 
+class ContactCreateRequest extends BaseFormRequest
+{
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'job_position' => 'required|string|max:255',
+            'email' => 'required|email',
+            'phone' => 'required|string|max:255',
+            'image' => 'required|string',
+            'position' => 'sometimes|nullable|integer',
+            'is_enabled' => 'sometimes|boolean',
+        ];
+    }
 }
