f0b37115397631c0500bd7e0e6299e461dd10494
ElijahCoding <cuiyeqing960904@gmail.com>
2022-05-26 15:54:24 +0300

    api built

--- Modules/Contact/Services/ContactStorage.php
+++ Modules/Contact/Services/ContactStorage.php
@@ -2,20 +2,31 @@
 
 namespace Modules\Contact\Services;
 
+use Modules\Contact\Dto\ContactDto;
+use Modules\Contact\Models\Contact;
+
 class ContactStorage
 {
-    public function store()
+    public function store(ContactDto $contactDto)
     {
-
+        return Contact::query()->create($contactDto->toArray());
     }
 
-    public function update()
+    public function update(Contact $contact, ContactDto $contactDto)
     {
+        $attributes = $contactDto->toArray();
 
+        if (!$contact->update($attributes)) {
+            throw new \LogicException('can not update contact');
+        }
+
+        return $contact;
     }
 
-    public function destroy()
+    public function destroy(Contact $contact)
     {
-
+        if (!$contact->delete()) {
+            throw new \LogicException('can not delete contact');
+        }
     }
 }
