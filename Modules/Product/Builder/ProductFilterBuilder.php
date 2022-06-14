f1e06c998b1729cbf51bdd07a5c73176b75d9c38
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-14 14:04:27 +0300

    testing product filter gql query

--- Modules/Product/Builder/ProductFilterBuilder.php
+++ Modules/Product/Builder/ProductFilterBuilder.php
@@ -3,11 +3,28 @@
 namespace Modules\Product\Builder;
 
 use GraphQL\Type\Definition\ResolveInfo;
+use Modules\Product\Http\Resources\FilteredProductResourceCollection;
+use Modules\Product\Services\ProductFilter;
 
 class ProductFilterBuilder
 {
-    public function getProductFilters($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    public function __construct(
+        protected ProductFilter $productFilter
+    ) {}
+
+    public function getProductFilters(
+        $rootValue, array $args,
+        GraphQLContext $context,
+        ResolveInfo $resolveInfo
+    ): FilteredProductResourceCollection
     {
-        dd($args);
+        $products = $this->productFilter
+            ->setFilters($args['filters'] ?? [])
+            ->setPage($args['page.number'] ?? 1)
+            ->setSize($args['page.size'] ?? 15)
+            ->setSort($args['orderBy'] ?? 'popular')
+            ->getItems();
+
+        return new FilteredProductResourceCollection($products);
     }
 }
