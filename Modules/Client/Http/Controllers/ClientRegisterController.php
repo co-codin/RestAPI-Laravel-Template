1328784bd7e104a343a0cb9220e2e5b7a3dd9879
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-16 14:47:19 +0300

    building requests

---
+++ Modules/Client/Http/Controllers/ClientRegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Client\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ClientRegisterController extends Controller
+{
+    public function fastRegister()
+    {
+
+    }
+}
---
+++ Modules/Client/Http/Requests/FastRegisterRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Client\Http\Requests;
+
+use App\Http\Requests\BaseFormRequest;
+
+class FastRegisterRequest extends BaseFormRequest
+{
+
+}
