8de95dd73344324177c8b2f008c0b895e505fe7c
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-20 18:26:42 +0300

    fixed

--- Modules/Client/Http/Requests/ClientVerifyCodeRequest.php
+++ Modules/Client/Http/Requests/ClientVerifyCodeRequest.php
@@ -6,5 +6,11 @@ use App\Http\Requests\BaseFormRequest;
 
 class ClientVerifyCodeRequest extends BaseFormRequest
 {
-
+    public function rules(): array
+    {
+        return [
+            'phone' => 'required|string|max:255|phone_default_countries|exists:clients,phone',
+            'code' => 'required|string|size:4',
+        ];
+    }
 }
