c4b1acfcefdc6a2211cead7c99c6278d3d6047c6
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-20 10:19:27 +0300

    building resource

--- Modules/Client/Http/Controllers/ClientPayerController.php
+++ Modules/Client/Http/Controllers/ClientPayerController.php
@@ -3,8 +3,20 @@
 namespace Modules\Client\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Modules\Client\Repositories\ClientPayerRepository;
+use Modules\Client\Repositories\ClientRepository;
+use Modules\Client\Services\ClientPayerStorage;
 
 class ClientPayerController extends Controller
 {
-    public function __construct(){}
+    public function __construct(
+        protected ClientPayerRepository $clientPayerRepository,
+        protected ClientRepository $clientRepository,
+        protected ClientPayerStorage $clientPayerStorage
+    ){}
+
+    public function index()
+    {
+
+    }
 }
---
+++ Modules/Client/Http/Resources/ClientPayerResource.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Client\Http\Resources;
+
+use App\Http\Resources\BaseJsonResource;
+
+class ClientPayerResource extends BaseJsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+
+        ];
+    }
+}
