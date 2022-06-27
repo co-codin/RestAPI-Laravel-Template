32eecc245c6bae82b1d9497e18da4eb9f3c99f86
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-28 01:00:32 +0300

    product hot view built

--- Modules/Product/Database/Migrations/2022_06_27_155524_create_products_hot_view.php
+++ Modules/Product/Database/Migrations/2022_06_27_155524_create_products_hot_view.php
@@ -10,6 +10,7 @@ class CreateProductsHotView extends Migration
      * @return void
      */
     public function up()
+//        AND pp.property_id = 259 AND WHERE json_contains('field_value_ids', 1)
     {
         DB::statement("CREATE VIEW products_hot_view AS
                         SELECT
@@ -19,19 +20,29 @@ class CreateProductsHotView extends Migration
                             fv.value AS stock_type_value,
                             pv.id AS variation_id, pv.price, pv.previous_price, pv.is_price_visible, pv.currency_id,
                             cur.rate AS currency_rate,
-                            img.image AS img
+                            img.image AS img,
+                            pr.ratings
+
 
                         FROM products as p
+
+
+
                         LEFT JOIN field_values AS fv ON p.stock_type_id = fv.id
                         LEFT JOIN product_category AS pc ON p.id = pc.product_id
                         LEFT JOIN categories AS c ON pc.category_id = c.id AND pc.is_main = 1
                         LEFT JOIN brands AS b ON p.brand_id = b.id
-                        LEFT JOIN product_variations AS pv ON p.id = pv.product_id
+                        LEFT JOIN product_variations AS pv ON p.id = pv.product_id AND pv.previous_price IS NOT NULL AND pv.price IS NOT NULL AND pv.is_price_visible = 1
                         LEFT JOIN currencies AS cur ON pv.currency_id = cur.id
-                        LEFT JOIN images as img ON p.id = img.imageable_id AND img.imageable_type = 'Modules\\Product\\Models\\Product'
-//                        LEFT JOIN product_reviews
-                        
-                        WHERE pc.is_main = 1 AND p.status = 1 AND p.group_id = 4 AND p.is_in_home = 1
+                        LEFT JOIN images AS img ON p.id = img.imageable_id AND img.imageable_type = 'Modules\\Product\\Models\\Product'
+                        LEFT JOIN product_reviews AS pr ON p.id = pr.product_id AND pr.status = 2
+                        LEFT JOIN product_questions AS pq ON p.id = pq.product_id AND pq.status = 2
+                        LEFT JOIN product_answers AS pa ON pa.product_question_id = pq.id
+
+
+                        WHERE
+                            NOT EXISTS (SELECT NULL FROM product_property AS pp WHERE pp.product_id = p.id AND pp.property_id = 259 AND field_value_ids = 1)
+                            AND pc.is_main = 1 AND p.status = 1 AND p.group_id = 4 AND p.is_in_home = 1
                         "
         );
     }
