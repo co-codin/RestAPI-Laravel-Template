4621b42e84bed4639cdf5b2cbc7853291756a0e4
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-27 17:39:28 +0300

    left joins

--- Modules/Product/Database/Migrations/2022_06_27_155524_create_products_hot_view.php
+++ Modules/Product/Database/Migrations/2022_06_27_155524_create_products_hot_view.php
@@ -12,8 +12,14 @@ class CreateProductsHotView extends Migration
     public function up()
     {
         DB::statement("CREATE VIEW products_hot_view AS
-                        SELECT *
-                        FROM companies AS c"
+                        SELECT p.*, fv.*
+                        FROM products as p
+                        LEFT JOIN field_values as fv ON p.stock_type_id = fv.id
+                        LEFT JOIN product_category as pc ON p.id = pc.product_id
+                        LEFT JOIN categories as c ON pc.category_id = c.id WHERE pc.is_main = 1
+
+                        WHERE pc.is_main = 1 AND p.status = 1 AND p.group_id = 4 AND p.is_in_home = 1
+                        "
         );
     }
 
--- Modules/Product/Models/Views/ProductHotView.php
+++ Modules/Product/Models/Views/ProductHotView.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductHotView extends Model
 {
-
+    protected $table = 'products_hot_view';
 }
