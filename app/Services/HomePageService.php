95e06d1df38c1ae3011aeb00e853d765fe7fb2c5
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-29 13:14:26 +0300

    products hot relationship

--- app/Services/HomePageService.php
+++ app/Services/HomePageService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Enums\Status;
+use Modules\Product\Enums\ProductGroup;
 use Modules\Product\Models\Product;
 
 class HomePageService
@@ -12,14 +14,44 @@ class HomePageService
         $productsRussiaBuilder = $this->getProductsRussia();
         $productsCovidBuilder = $this->getProductsCovid();
 
-
-
-        return $productsHotBuilder->merge($productsRussiaBuilder, $productsCovidBuilder)->all();
+        return $productsHotBuilder->merge([
+            $productsRussiaBuilder, $productsCovidBuilder,
+        ])->all();
     }
 
     public function getProductsHot()
     {
-        return Product::query()->get();
+        return Product::query()
+            ->where([
+                ['is_in_home', '=', true],
+                ['status', '=', Status::ACTIVE],
+                ['group_id', '=', ProductGroup::IMPOSSIBLE],
+            ])
+            ->with(
+                ['stockType' => function ($query) {
+                    $query->select('value');
+                }],
+                ['category' => function ($query) {
+                    $query->select('name');
+                }],
+                ['brand' => function ($query) {
+                    $query->select('name');
+                }],
+                ['mainVariation' => function ($query) {
+                    $query->select(['id', 'price', 'previous_price', 'is_price_visible', 'currency_id', 'stock_type']);
+                }],
+                ['mainVariation.currency' => function ($query) {
+                    $query->select('rate');
+                }],
+                ['images' => function ($query) {
+                    $query->select('image');
+                }],
+                ['productReviews' => function ($query) {
+                    $query->select('ratings');
+                }],
+            )
+            ->take(20)
+            ->get();
     }
 
     public function getProductsRussia()
