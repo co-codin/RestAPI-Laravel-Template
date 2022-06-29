29555e87ac4c01079f8133225efd8df4cd733a24
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-29 12:43:20 +0300

    structure building

---
+++ app/Services/HomePageService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+class HomePageService
+{
+    public function getProductsHot()
+    {
+
+    }
+
+    public function getProductsRussia()
+    {
+
+    }
+
+    public function getProductsCovid()
+    {
+
+    }
+
+    public function getBrands()
+    {
+
+    }
+
+    public function getBanners()
+    {
+
+    }
+
+    public function getPublications()
+    {
+
+    }
+
+    public function getNews()
+    {
+
+    }
+}
