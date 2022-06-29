3af6ce562fde29b423fa17c7791bbabd607b4c0b
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-29 15:53:17 +0300

    checked

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\HomePageResource;
 use App\Services\HomePageService;
 
 class HomePageController extends Controller
@@ -12,6 +13,6 @@ class HomePageController extends Controller
 
     public function index()
     {
-        return $this->homePageService->getBrands();
+        return HomePageResource::collection($this->homePageService->all());
     }
 }
