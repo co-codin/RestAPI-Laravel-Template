43a86434f6afcc67b87b10a3ec71dab1d99c9898
ElijahCoding <cuiyeqing960904@gmail.com>
2022-06-29 12:50:01 +0300

    structure built

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\HomePageService;
+
 class HomePageController extends Controller
 {
+    public function __construct(
+        protected HomePageService $homePageService
+    ) {}
+
     public function index()
     {
-
+        return $this->homePageService->all();
     }
 }
--- app/Services/HomePageService.php
+++ app/Services/HomePageService.php
@@ -2,21 +2,34 @@
 
 namespace App\Services;
 
+use Modules\Product\Models\Product;
+
 class HomePageService
 {
+    public function all()
+    {
+        $productsHotBuilder = $this->getProductsHot();
+        $productsRussiaBuilder = $this->getProductsRussia();
+        $productsCovidBuilder = $this->getProductsCovid();
+
+
+
+        return $productsHotBuilder->merge($productsRussiaBuilder, $productsCovidBuilder)->all();
+    }
+
     public function getProductsHot()
     {
-
+        return Product::query()->get();
     }
 
     public function getProductsRussia()
     {
-
+        return Product::query()->get();
     }
 
     public function getProductsCovid()
     {
-
+        return Product::query()->get();
     }
 
     public function getBrands()
