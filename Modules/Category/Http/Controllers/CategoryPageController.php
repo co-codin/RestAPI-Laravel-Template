4fdd2a9261d18c135b5b38f095a25646b72d7b48
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-06 13:25:02 +0300

    refactored

--- Modules/Category/Http/Controllers/CategoryPageController.php
+++ Modules/Category/Http/Controllers/CategoryPageController.php
@@ -6,6 +6,7 @@ use App\Enums\Status;
 use App\Http\Controllers\Controller;
 use Modules\Category\Http\Resources\CategoryPageResource;
 use Modules\Category\Repositories\CategoryRepository;
+use Modules\Category\Repositories\Criteria\CategoryPageCriteria;
 use Modules\Category\Services\CategoryBrandsField;
 
 class CategoryPageController extends Controller
@@ -38,36 +39,7 @@ class CategoryPageController extends Controller
     public function show(string $category)
     {
         $category = $this->categoryRepository
-            ->scopeQuery(function ($query) use ($category) {
-                return $query
-                    ->addSelect('id', 'name', 'slug', 'image', 'full_description', 'parent_id', '_lft', '_rgt')
-                ;
-            })
-            ->with([
-                'ancestors' => function ($query) {
-                    $query->addSelect('id', 'parent_id', 'name', 'slug', '_lft', '_rgt');
-                }
-            ])
-            ->with([
-                'descendants' => function ($query) {
-                    $query->addSelect('id', 'parent_id', '_lft', '_rgt');
-                }
-            ])
-            ->with([
-                'children' => function ($query) {
-                    $query->addSelect('id', 'name', 'slug', 'image', 'status', '_lft', 'parent_id', '_lft', '_rgt');
-                }
-            ])
-            ->with(['seo' => function ($query) {
-                $query->addSelect('seoable_id', 'title', 'description', 'h1', 'is_enabled');
-            }])
-            ->with([
-                'filters' => function ($query) {
-                    $query->with(['property' => function ($query) {
-                        $query->addSelect('id', 'key');
-                    }]);
-                }
-            ])
+            ->pushCriteria(CategoryPageCriteria::class)
             ->findByField('slug', $category)
             ->first();
 
