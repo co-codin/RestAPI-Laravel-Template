ef0d74ab222b0a51a47a2acfbf61c7951666536c
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-07 15:44:53 +0300

    contacts page api

--- Modules/Contact/Routes/api.php
+++ Modules/Contact/Routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
 use Modules\Contact\Http\Controllers\ContactController;
+use Modules\Contact\Http\Controllers\ContactPageController;
+
+Route::get('page/contacts', [ContactPageController::class, 'index']);
 
 Route::resource('contacts', ContactController::class)->only(['index', 'show']);
