821b689e7003ec7a7612c85df00d773c9290f05e
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-11 18:12:35 +0300

    page refactored

--- Modules/Page/Routes/api.php
+++ Modules/Page/Routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Page\Http\Controllers\PageController;
+use Modules\Page\Http\Controllers\PagePageController;
+
+Route::get('page/pages/{page}', [PagePageController::class, 'show']);
 
 Route::get('pages/all', [PageController::class, 'all'])->name('pages.all');
 Route::resource('pages', PageController::class)->only(['index', 'show']);
