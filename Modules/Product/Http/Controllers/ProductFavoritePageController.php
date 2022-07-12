17ae29236b8944b8cfcf4c32a7704b70f7c1d52b
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-12 23:40:46 +0300

    fixed

--- Modules/Product/Http/Controllers/ProductFavoritePageController.php
+++ Modules/Product/Http/Controllers/ProductFavoritePageController.php
@@ -3,6 +3,7 @@
 namespace Modules\Product\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Modules\Category\Models\Category;
 use Modules\Product\Http\Resources\ProductFavoritePageResource;
 use Modules\Product\Repositories\Criteria\ProductFavoritePageCriteria;
 use Modules\Product\Repositories\ProductRepository;
@@ -22,19 +23,37 @@ class ProductFavoritePageController extends Controller
     {
         $productIds = request()->get('ids');
         $categoryId = request()->get('category_id');
-        $priceSort = request()->get('price_sort');
+        $priceSort = request()->get('price_sort') ?? 'ASC';
         $page = request()->get('page');
         $perPage = request()->get('per_page');
 
         $products = $this->productRepository
-            ->scopeQuery(function ($query) {
-                return $query
+            ->scopeQuery(function ($query) use ($categoryId, $productIds) {
+                $query = $query
+                    ->whereIn('id', $productIds)
                     ->withPrice()
                     ->withMainVariation()
                     ;
+
+                if ($categoryId) {
+                    $rootCategory = Category::findOrFail($categoryId);
+
+                    $categoryIds = $rootCategory->descendants()
+                        ->pluck('id')
+                        ->add($rootCategory->id);
+
+                    $query = $query
+                        ->whereHas(
+                            'productCategories',
+                            fn($builder) => $builder->whereIn('category_id', $categoryIds)
+                        );
+                }
+                return $query;
             })
+            ->orderBy('price', $priceSort)
+            ->paginate($perPage, $page)
             ;
 
-        return ProductFavoritePageResource::collection($productIds);
+        return ProductFavoritePageResource::collection($products);
     }
 }
