cdd6a9e88f2815f7522d5736789d26595133a661
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-13 17:20:23 +0300

    role crud

--- Modules/Role/Http/Controllers/Admin/RoleController.php
+++ Modules/Role/Http/Controllers/Admin/RoleController.php
@@ -3,23 +3,27 @@
 namespace Modules\Role\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Modules\Role\Http\Requests\RoleCreateRequest;
+use Modules\Role\Http\Requests\RoleUpdateRequest;
 use Modules\Role\Models\Role;
+use Modules\Role\Repositories\RoleRepository;
 use Modules\Role\Services\RoleStorage;
 
 class RoleController extends Controller
 {
     public function __construct(
-        protected RoleStorage $roleStorage
+        protected RoleStorage $roleStorage,
+        protected RoleRepository $roleRepository
     ) {
         $this->authorizeResource(Role::class, 'role');
     }
 
-    public function store()
+    public function store(RoleCreateRequest $request)
     {
 
     }
 
-    public function update()
+    public function update(RoleUpdateRequest $request)
     {
 
     }
--- Modules/Role/Services/RoleStorage.php
+++ Modules/Role/Services/RoleStorage.php
@@ -2,15 +2,29 @@
 
 namespace Modules\Role\Services;
 
+use Modules\Role\Dto\RoleDto;
+use Modules\Role\Models\Role;
+
 class RoleStorage
 {
-    public function store()
+    public function store(RoleDto $dto)
     {
-
+        return Role::query()->create($dto->toArray());
     }
 
-    public function update()
+    public function update(Role $role, RoleDto $dto)
     {
+        if (!$role->update($dto->toArray())) {
+            throw new \LogicException('can not update role');
+        }
 
+        return $role;
+    }
+
+    public function delete(Role $role)
+    {
+        if (!$role->delete()) {
+            throw new \LogicException('can not delete role');
+        }
     }
 }
