a31f59eef853411999d707f5df495bf34f0cac4a
Lenar Khusainov <l.khusainov@medeq.ru>
2022-07-13 12:09:50 +0300

    Add application pdf headers

--- app/Http/Controllers/DownloadFileController.php
+++ app/Http/Controllers/DownloadFileController.php
@@ -9,8 +9,12 @@ class DownloadFileController extends \Illuminate\Routing\Controller
         abort_unless(request()->has('file'), 404);
 
         try {
-            $file = \Storage::disk('s3')->download(request()->input('file'));
+            $file = \Storage::disk('s3')->response(request()->input('file'), null, [
+                'Content-Type' => 'application/pdf'
+            ]);
+
             throw_if(!$file);
+
             return $file;
         }
         catch (\Throwable) {
