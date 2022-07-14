feaa9646f02627340d014a0a61618da63666708e
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-14 11:44:47 +0300

    role crud built

--- Modules/Role/Http/Controllers/Admin/RoleController.php
+++ Modules/Role/Http/Controllers/Admin/RoleController.php
@@ -3,11 +3,13 @@
 namespace Modules\Role\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Modules\Role\Dto\RoleDto;
 use Modules\Role\Http\Requests\RoleCreateRequest;
 use Modules\Role\Http\Requests\RoleUpdateRequest;
 use Modules\Role\Models\Role;
 use Modules\Role\Repositories\RoleRepository;
 use Modules\Role\Services\RoleStorage;
+use Modules\Role\Http\Resources\RoleResource;
 
 class RoleController extends Controller
 {
@@ -20,16 +22,26 @@ class RoleController extends Controller
 
     public function store(RoleCreateRequest $request)
     {
+        $role = $this->roleStorage->store(RoleDto::fromFormRequest($request));
 
+        return new RoleResource($role);
     }
 
-    public function update(RoleUpdateRequest $request)
+    public function update(int $role, RoleUpdateRequest $request)
     {
+        $role = $this->roleRepository->find($role);
 
+        $role = $this->roleStorage->update($role, RoleDto::fromFormRequest($request));
+
+        return new RoleResource($role);
     }
 
-    public function destroy()
+    public function destroy(int $role)
     {
+        $role = $this->roleRepository->find($role);
 
+        $this->roleStorage->delete($role);
+
+        return response()->noContent();
     }
 }
--- Modules/Role/Routes/admin.php
+++ Modules/Role/Routes/admin.php
@@ -1,1 +1,5 @@
 <?php
+
+use Modules\Role\Http\Controllers\Admin\RoleController;
+
+Route::resource('roles', RoleController::class)->except('index', 'show');
