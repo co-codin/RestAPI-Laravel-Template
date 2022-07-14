a4ad84c3a87627f672421fc0a07b48a0254ac534
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-14 12:07:23 +0300

    testing on achievement

---
+++ Modules/Achievement/Enums/AchievementPermission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Achievement\Enums;
+
+use Modules\Role\Contracts\PermissionEnum;
+
+final class AchievementPermission implements PermissionEnum
+{
+    const CREATE_ACHIEVEMENTS = 'create achievements';
+    const VIEW_ACHIEVEMENTS = 'view achievements';
+    const EDIT_ACHIEVEMENTS = 'edit achievements';
+    const DELETE_ACHIEVEMENTS = 'delete achievements';
+
+    public static function descriptions() : array
+    {
+        return [
+            self::CREATE_ACHIEVEMENTS => 'Создание достижений',
+            self::VIEW_ACHIEVEMENTS => 'Просмотр достижений',
+            self::EDIT_ACHIEVEMENTS => 'Редактирование достижений',
+            self::DELETE_ACHIEVEMENTS => 'Удаление достижений',
+        ];
+    }
+}
