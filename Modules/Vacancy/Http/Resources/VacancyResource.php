bb5bd09bcc43412d9800b894ee071eb33769b305
Lenar Khusainov <work@lenarx.ru>
2022-07-18 12:05:53 +0300

    Fix status enum in vacancies

--- Modules/Vacancy/Http/Resources/VacancyResource.php
+++ Modules/Vacancy/Http/Resources/VacancyResource.php
@@ -6,13 +6,22 @@ namespace Modules\Vacancy\Http\Resources;
 
 use App\Enums\Status;
 use App\Http\Resources\BaseJsonResource;
+use Modules\Vacancy\Models\Vacancy;
 
+/**
+ * Class VacancyResource
+ * @package Modules\Vacancy\Http\Resources
+ * @mixin Vacancy
+ */
 class VacancyResource extends BaseJsonResource
 {
     public function toArray($request): array
     {
         return array_merge(parent::toArray($request), [
-            'status' => $this->whenRequested('status', Status::fromValue($this->status)),
+            'status' => $this->whenRequested(
+                'status',
+                Status::fromValue($this->status)->toArray()
+            ),
         ]);
     }
 }
--- Modules/Vacancy/Models/Vacancy.php
+++ Modules/Vacancy/Models/Vacancy.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Modules\Vacancy\Database\factories\VacancyFactory;
 
+/**
+ * Class Vacancy
+ * @package Modules\Vacancy\Models
+ * @property string $name
+ * @property int $status
+ */
 class Vacancy extends Model
 {
     use HasFactory, SoftDeletes, IsActive;
