a6c35f40164b0fef8676356930865af84a276c7e
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-18 17:07:33 +0300

    building faq

---
+++ Modules/Faq/Enums/QuestionCategoryPermission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Faq\Enums;
+
+use App\Enums\BaseEnum;
+
+class QuestionCategoryPermission extends BaseEnum
+{
+    const CREATE_QUESTION_CATEGORIES = 'create question categories';
+    const VIEW_QUESTION_CATEGORIES = 'view question categories';
+    const EDIT_QUESTION_CATEGORIES = 'edit question categories';
+    const DELETE_QUESTION_CATEGORIES = 'delete question categories';
+
+    public static function descriptions() : array
+    {
+        return [
+            static::CREATE_QUESTION_CATEGORIES => 'Создание категорий вопросов',
+            static::VIEW_QUESTION_CATEGORIES => 'Просмотр категорий вопросов',
+            static::EDIT_QUESTION_CATEGORIES => 'Редактирование категорий вопросов',
+            static::DELETE_QUESTION_CATEGORIES => 'Удаление категорий вопросов',
+        ];
+    }
+}
