3fc8a4622deeac776df3c9aa7091407330dd03ed
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-18 23:00:10 +0300

    building policies

---
+++ Modules/Product/Policies/ProductAnswerPolicy.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Product\Policies;
+
+use App\Policies\BasePolicy;
+
+class ProductAnswerPolicy extends BasePolicy
+{
+
+}
---
+++ Modules/Product/Policies/ProductQuestionPolicy.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Product\Policies;
+
+use App\Policies\BasePolicy;
+
+class ProductQuestionPolicy extends BasePolicy
+{
+
+}
