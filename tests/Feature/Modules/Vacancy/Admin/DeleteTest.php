f4db7b2646497f2488ad48141a5fab8cd5e0bf49
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-20 00:58:48 +0300

    fixing vacancy delete endpoint

--- tests/Feature/Modules/Vacancy/Admin/DeleteTest.php
+++ tests/Feature/Modules/Vacancy/Admin/DeleteTest.php
@@ -4,18 +4,41 @@
 namespace Tests\Feature\Modules\Vacancy\Admin;
 
 
+use Modules\Role\Models\Permission;
+use Modules\User\Models\User;
+use Modules\Vacancy\Enums\VacancyPermission;
 use Modules\Vacancy\Models\Vacancy;
 use Tests\TestCase;
 
 class DeleteTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create([
+            'email' => 'admin@medeq.ru'
+        ]);
+
+        $deletePermission = Permission::factory()->create([
+            'name' => VacancyPermission::DELETE_VACANCIES
+        ]);
+
+        $user->givePermissionTo($deletePermission->name);
+
+        $response = $this->json('POST', route('auth.login'), [
+            'email' => 'admin@medeq.ru',
+            'password' => 'admin1',
+        ]);
+
+        $this->withToken($response->json('token'));
+    }
+
     public function test_authenticated_can_delete_vacancy()
     {
-        $this->authenticateUser();
-
         $vacancy = Vacancy::factory()->create();
 
-        $response = $this->deleteJson(route('admin.vacancies.destroy', $vacancy));
+        $response = $this->json('DELETE', route('admin.vacancies.destroy', $vacancy));
 
         $response->assertNoContent();
 
