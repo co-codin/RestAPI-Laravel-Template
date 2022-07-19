af4e5323f3aae8ffa661952b8aa73c2e8c1d742d
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-19 16:25:42 +0300

    fixing

--- Modules/Role/Http/Controllers/Admin/RoleController.php
+++ Modules/Role/Http/Controllers/Admin/RoleController.php
@@ -7,41 +7,33 @@ use Modules\Role\Dto\RoleDto;
 use Modules\Role\Http\Requests\RoleCreateRequest;
 use Modules\Role\Http\Requests\RoleUpdateRequest;
 use Modules\Role\Models\Role;
-use Modules\Role\Repositories\RoleRepository;
 use Modules\Role\Services\RoleStorage;
 use Modules\Role\Http\Resources\RoleResource;
 
 class RoleController extends Controller
 {
     public function __construct(
-        protected RoleStorage $roleStorage,
-        protected RoleRepository $roleRepository
-    ) {}
+        protected RoleStorage $roleStorage
+    ) {
+        $this->authorizeResource(Role::class, 'role');
+    }
 
     public function store(RoleCreateRequest $request)
     {
-        $this->authorize('create', Role::class);
-
         $role = $this->roleStorage->store(RoleDto::fromFormRequest($request));
 
         return new RoleResource($role);
     }
 
-    public function update(int $role, RoleUpdateRequest $request)
+    public function update(Role $role, RoleUpdateRequest $request)
     {
-        $role = $this->roleRepository->find($role);
-
-        $this->authorize('update', $role);
-
         $role = $this->roleStorage->update($role, RoleDto::fromFormRequest($request));
 
         return new RoleResource($role);
     }
 
-    public function destroy(int $role)
+    public function destroy(Role $role)
     {
-        $role = $this->roleRepository->find($role);
-
         $this->authorize('delete', $role);
 
         $this->roleStorage->delete($role);
