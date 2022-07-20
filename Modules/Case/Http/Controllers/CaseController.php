8eddc88fa8b843adb2aea00f158b13d0528edc17
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-20 15:41:45 +0300

    case fixed

--- Modules/Case/Http/Controllers/CaseController.php
+++ Modules/Case/Http/Controllers/CaseController.php
@@ -11,19 +11,23 @@ class CaseController extends Controller
 {
     public function __construct(
         protected CaseRepository $caseRepository
-    ) {
-        $this->authorizeResource(CaseModel::class, 'case');
-    }
+    ) {}
 
     public function index()
     {
+        $this->authorize('viewAny', CaseModel::class);
+
         $cases = $this->caseRepository->jsonPaginate();
 
         return CaseResource::collection($cases);
     }
 
-    public function show(CaseModel $case)
+    public function show(int $case)
     {
+        $case = $this->caseRepository->find($case);
+
+        $this->authorize('view', $case);
+
         return new CaseResource($case);
     }
 }
