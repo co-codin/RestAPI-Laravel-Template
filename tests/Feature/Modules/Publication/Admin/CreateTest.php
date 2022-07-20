cec1a3366a418933fe629e365e780ddc1ec403fd
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-20 22:47:54 +0300

    publication tests

--- tests/Feature/Modules/Publication/Admin/CreateTest.php
+++ tests/Feature/Modules/Publication/Admin/CreateTest.php
@@ -4,15 +4,38 @@
 namespace Tests\Feature\Modules\Publication\Admin;
 
 use Illuminate\Support\Facades\Storage;
+use Modules\Publication\Enums\PublicationPermission;
 use Modules\Publication\Models\Publication;
+use Modules\Role\Models\Permission;
+use Modules\User\Models\User;
 use Tests\TestCase;
 
 class CreateTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create([
+            'email' => 'admin@medeq.ru'
+        ]);
+
+        $permission = Permission::factory()->create([
+            'name' => PublicationPermission::CREATE_PUBLICATIONS
+        ]);
+
+        $user->givePermissionTo($permission->name);
+
+        $response = $this->json('POST', route('auth.login'), [
+            'email' => 'admin@medeq.ru',
+            'password' => 'admin1',
+        ]);
+
+        $this->withToken($response->json('token'));
+    }
+
     public function test_authenticated_can_create_publication()
     {
-        $this->authenticateUser();
-
         $publicationData = Publication::factory()->raw();
 
         $response = $this->json('POST', route('admin.publications.store'), $publicationData);
--- tests/Feature/Modules/Publication/Admin/UpdateTest.php
+++ tests/Feature/Modules/Publication/Admin/UpdateTest.php
@@ -3,15 +3,38 @@
 
 namespace Tests\Feature\Modules\Publication\Admin;
 
+use Modules\Publication\Enums\PublicationPermission;
 use Modules\Publication\Models\Publication;
+use Modules\Role\Models\Permission;
+use Modules\User\Models\User;
 use Tests\TestCase;
 
 class UpdateTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create([
+            'email' => 'admin@medeq.ru'
+        ]);
+
+        $permission = Permission::factory()->create([
+            'name' => PublicationPermission::EDIT_PUBLICATIONS
+        ]);
+
+        $user->givePermissionTo($permission->name);
+
+        $response = $this->json('POST', route('auth.login'), [
+            'email' => 'admin@medeq.ru',
+            'password' => 'admin1',
+        ]);
+
+        $this->withToken($response->json('token'));
+    }
+
     public function test_authenticated_can_update_publication()
     {
-        $this->authenticateUser();
-
         $publication = Publication::factory()->create([
             'is_enabled' => true,
         ]);
