ee61fb40bfb4745e1b235db4bcf5f3b55d3ba0ff
ElijahCoding <cuiyeqing960904@gmail.com>
2022-07-20 22:50:46 +0300

    property tested

--- tests/Feature/Modules/Property/Admin/UpdateTest.php
+++ tests/Feature/Modules/Property/Admin/UpdateTest.php
@@ -4,15 +4,38 @@
 namespace Tests\Feature\Modules\Property\Admin;
 
 
+use Modules\Property\Enums\PropertyPermission;
 use Modules\Property\Models\Property;
+use Modules\Role\Models\Permission;
+use Modules\User\Models\User;
 use Tests\TestCase;
 
 class UpdateTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create([
+            'email' => 'admin@medeq.ru'
+        ]);
+
+        $permission = Permission::factory()->create([
+            'name' => PropertyPermission::EDIT_PROPERTIES
+        ]);
+
+        $user->givePermissionTo($permission->name);
+
+        $response = $this->json('POST', route('auth.login'), [
+            'email' => 'admin@medeq.ru',
+            'password' => 'admin1',
+        ]);
+
+        $this->withToken($response->json('token'));
+    }
+
     public function test_authenticated_can_update_a_property()
     {
-        $this->authenticateUser();
-
         $property = Property::factory()->create();
 
         $response = $this->json('PATCH', route('admin.properties.update', $property), [
