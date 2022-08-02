b6d840fa3136a306b26a8861e2a8315367c6d285
Lenar Khusainov <work@lenarx.ru>
2022-08-02 14:43:04 +0400

    Fix case graphql

--- Modules/Seo/Http/Controllers/Admin/SeoRuleSeoController.php
+++ Modules/Seo/Http/Controllers/Admin/SeoRuleSeoController.php
@@ -9,16 +9,20 @@ use Modules\Seo\Dto\SeoDto;
 use Modules\Seo\Http\Requests\Admin\SeoUpdateRequest;
 use Modules\Seo\Http\Resources\SeoResource;
 use Modules\Seo\Models\SeoRule;
+use Modules\Seo\Repositories\SeoRuleRepository;
 use Modules\Seo\Services\SeoStorage;
 
 class SeoRuleSeoController extends Controller
 {
     public function __construct(
         protected SeoStorage $seoStorage,
+        protected SeoRuleRepository $seoRuleRepository
     ) {}
 
-    public function update(SeoUpdateRequest $request, SeoRule $seo_rule)
+    public function update(SeoUpdateRequest $request, int $seo_rule)
     {
+        $seo_rule = $this->seoRuleRepository->find($seo_rule);
+
         $this->authorize('update', $seo_rule);
 
         $seo = $this->seoStorage->update(
